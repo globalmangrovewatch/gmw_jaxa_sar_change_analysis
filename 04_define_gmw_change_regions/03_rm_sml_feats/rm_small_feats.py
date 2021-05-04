b400a723fab246b929932f03960271feffeceb50
Pete Bunting <petebunting@mac.com>
2021-05-04 15:52:03 +0100

    bug fix so histogram is calculated.

--- 04_define_gmw_change_regions/03_rm_sml_feats/rm_small_feats.py
+++ 04_define_gmw_change_regions/03_rm_sml_feats/rm_small_feats.py
@@ -23,21 +23,21 @@ class RMSmallPotentChangeFeatures(PBPTQProcessTool):
         inverse_init_msk = os.path.join(self.params['tmp_dir'], '{}_inv_init_msk.kea')
         band_defns = [rsgislib.imagecalc.BandDefn('chg_msk', self.params['gmw_pot_chng_rgns_img'], 1)]
         rsgislib.imagecalc.bandMath(inverse_init_msk, 'chg_msk==1?0:1', 'KEA', rsgislib.TYPE_8UINT, band_defns)
-        rsgislib.rastergis.populateStats(inverse_init_msk, addclrtab=True, calcpyramids=True, ignorezero=True)
 
         inverse_init_msk_clumps = os.path.join(self.params['tmp_dir'], '{}_inv_init_msk_clumps.kea')
         rsgislib.segmentation.clump(inverse_init_msk, inverse_init_msk_clumps, 'KEA', False, 0, False)
 
         inverse_init_msk_clumps_rmsml = os.path.join(self.params['tmp_dir'], '{}_inv_init_msk_clumps_rmsml.kea')
         rsgislib.segmentation.rmSmallClumps(inverse_init_msk_clumps, inverse_init_msk_clumps_rmsml, 10, 'KEA')
+        rsgislib.rastergis.populateStats(inverse_init_msk_clumps_rmsml, addclrtab=False, calcpyramids=False, ignorezero=True)
 
         init_msk_fill_sml = os.path.join(self.params['tmp_dir'], '{}_init_msk_fill_sml.kea')
         band_defns = [rsgislib.imagecalc.BandDefn('chg_msk', inverse_init_msk_clumps_rmsml, 1)]
         rsgislib.imagecalc.bandMath(init_msk_fill_sml, 'chg_msk==0?1:0', 'KEA', rsgislib.TYPE_16INT, band_defns)
-        rsgislib.rastergis.populateStats(init_msk_fill_sml, addclrtab=True, calcpyramids=True, ignorezero=True)
 
         init_msk_fill_clumps = os.path.join(self.params['tmp_dir'], '{}init_msk_fill_sml_clumps.kea')
         rsgislib.segmentation.clump(init_msk_fill_sml, init_msk_fill_clumps, 'KEA', False, 0, False)
+        rsgislib.rastergis.populateStats(init_msk_fill_clumps, addclrtab=False, calcpyramids=False, ignorezero=True)
 
         init_msk_fill_clumps_rmsml = os.path.join(self.params['tmp_dir'], '{}init_msk_fill_sml_clumps_rmsml.kea')
         rsgislib.segmentation.rmSmallClumps(init_msk_fill_clumps, init_msk_fill_clumps_rmsml, 3, 'KEA')
