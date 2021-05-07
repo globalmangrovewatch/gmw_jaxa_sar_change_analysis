ffe58c41b0768f12339a906076e0b5eee80ed656
Pete Bunting <petebunting@mac.com>
2021-05-07 10:01:52 +0100

    Add scripts to merge potential change regions.

---
+++ 04_define_gmw_change_regions/06_msk_mng_potntial_chng_regions/gen_cmds.py
@@ -0,0 +1,58 @@
+from pbprocesstools.pbpt_q_process import PBPTGenQProcessToolCmds
+import logging
+import os
+import glob
+import pathlib
+
+logger = logging.getLogger(__name__)
+
+class GenCmds(PBPTGenQProcessToolCmds):
+
+    def gen_command_info(self, **kwargs):
+        img_tiles = glob.glob(kwargs['gmw_tiles'])
+        for gmw_tile in img_tiles:
+            basename = self.get_file_basename(gmw_tile)
+            tile_basename = self.get_file_basename(gmw_tile, n_comps=2)
+
+            pot_ocean_chng_img = os.path.join(kwargs['pot_jaxa_chng_tiles_dir'], '{}_pot_chng_ocean_chg_rgns.kea'.format(tile_basename))
+            pot_sar_chng_img = os.path.join(kwargs['pot_jaxa_chng_tiles_dir'], '{}_v3_init_chng_rgns_rmsml.kea'.format(tile_basename))
+
+            out_img = os.path.join(kwargs['out_dir'], '{}_gmw_v3_pot_chng_ocean_chg_rgns.kea'.format(tile_basename))
+
+            if (not os.path.exists(out_img)):
+                c_dict = dict()
+                c_dict['tile'] = tile_basename
+                c_dict['gmw_tile'] = gmw_tile
+                c_dict['pot_ocean_chng'] = pot_ocean_chng_img
+                c_dict['pot_sar_chng'] = pot_sar_chng_img
+                c_dict['out_img'] = out_img
+                c_dict['tmp_dir'] = os.path.join(kwargs['tmp_dir'], "{}_pot_chng_ocean_chg_rgns".format(tile_basename))
+                if not os.path.exists(c_dict['tmp_dir']):
+                    os.mkdir(c_dict['tmp_dir'])
+                self.params.append(c_dict)
+
+
+    def run_gen_commands(self):
+        self.gen_command_info(gmw_tiles='/scratch/a.pfb/gmw_v2_gapfill/data/gmw_tiles/gmw_init_v3_qa/*.kea',
+                              pot_chng_regs_tiles_dir='/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/pot_chng_ocean_chg_rgns',
+                              pot_jaxa_chng_tiles_dir='/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_v3_init_change_regions_rm_sml',
+                              out_dir='/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/pot_gmw_chng_ocean_chg_rgns',
+                              tmp_dir='/scratch/a.pfb/gmw_v3_change/tmp')
+        self.pop_params_db()
+        self.create_slurm_sub_sh("pot_gmw_chng_ocean_chg_rgns", 16448, '/scratch/a.pfb/gmw_v3_change/logs',
+                                 run_script='run_exe_analysis.sh', job_dir="job_scripts",
+                                 db_info_file=None, account_name='scw1376', n_cores_per_job=10, n_jobs=10,
+                                 job_time_limit='2-23:59',
+                                 module_load='module load parallel singularity\n\nexport http_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\nexport https_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\n')
+
+if __name__ == "__main__":
+    py_script = os.path.abspath("create_img_tile.py")
+    script_cmd = "singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python {}".format(py_script)
+
+    process_tools_mod = 'create_img_tile'
+    process_tools_cls = 'CreateImageTile'
+
+    create_tools = GenCmds(cmd=script_cmd, db_conn_file="/home/a.pfb/gmw_gap_fill_db/pbpt_db_conn.txt",
+                                         lock_file_path="./gmw_gapfill_lock_file.txt",
+                                         process_tools_mod=process_tools_mod, process_tools_cls=process_tools_cls)
+    create_tools.parse_cmds()
