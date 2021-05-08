22c115c81d5a10205add36a9feae9141b44ee917
Pete Bunting <petebunting@mac.com>
2021-05-08 16:32:21 +0100

    Add scripts for adding additional regions to the potential change areas.

---
+++ 04_define_gmw_change_regions/07_additional_chng_regions/create_img_tile.py
@@ -0,0 +1,54 @@
+from pbprocesstools.pbpt_q_process import PBPTQProcessTool
+import logging
+import os
+import shutil
+import rsgislib
+import rsgislib.imagecalc
+import rsgislib.vectorutils
+import rsgislib.rastergis
+
+logger = logging.getLogger(__name__)
+
+
+class CreateImageTile(PBPTQProcessTool):
+
+    def __init__(self):
+        super().__init__(cmd_name='create_img_tile.py', descript=None)
+
+    def do_processing(self, **kwargs):
+        if not os.path.exists(self.params['tmp_dir']):
+            os.mkdir(self.params['tmp_dir'])
+
+        band_defns = [rsgislib.imagecalc.BandDefn('pot_chng', self.params['pot_chng_rgns_img'], 1),
+                      rsgislib.imagecalc.BandDefn('nasa_chng', self.params['nasa_chng_img'], 1),
+                      rsgislib.imagecalc.BandDefn('gmw_v2_chng', self.params['gmw_v2_union_img'], 1)]
+        rsgislib.imagecalc.bandMath(self.params['out_img'], '(pot_chng == 1) || (nasa_chng==1) || (gmw_v2_chng==1)?1:0', 'KEA', rsgislib.TYPE_8UINT, band_defns)
+        rsgislib.rastergis.populateStats(self.params['out_img'], addclrtab=True, calcpyramids=True, ignorezero=True)
+
+        if os.path.exists(self.params['tmp_dir']):
+            shutil.rmtree(self.params['tmp_dir'])
+
+
+    def required_fields(self, **kwargs):
+        return ["tile", "gmw_tile", "pot_chng_rgns_img", "nasa_chng_img", "gmw_v2_union_img", "out_img", "tmp_dir"]
+
+
+    def outputs_present(self, **kwargs):
+        files_dict = dict()
+        files_dict[self.params['out_img']] = 'gdal_image'
+        return self.check_files(files_dict)
+
+    def remove_outputs(self, **kwargs):
+        # Remove the output files.
+        if os.path.exists(self.params['out_img']):
+            os.remove(self.params['out_img'])
+
+        # Reset the tmp dir
+        if os.path.exists(self.params['tmp_dir']):
+            shutil.rmtree(self.params['tmp_dir'])
+        os.mkdir(self.params['tmp_dir'])
+
+if __name__ == "__main__":
+    CreateImageTile().std_run()
+
+
