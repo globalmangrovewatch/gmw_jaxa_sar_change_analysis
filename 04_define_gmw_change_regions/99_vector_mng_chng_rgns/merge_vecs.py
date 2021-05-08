463d1487932e1fc08e2fe92716a601f25a76ced0
Pete Bunting <petebunting@mac.com>
2021-05-08 17:14:08 +0100

    Update to polygonise alternative set of potential change regions.

--- 04_define_gmw_change_regions/99_vector_mng_chng_rgns/merge_vecs.py
+++ 04_define_gmw_change_regions/99_vector_mng_chng_rgns/merge_vecs.py
@@ -1,10 +1,10 @@
 import glob
 import rsgislib.vectorutils
 
-input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/pot_gmw_chng_ocean_chg_rgns_vecs/*.gpkg")
+input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/pot_gmw_chng_ocean_chg_rgns_nasa_gmwv2_updates_vecs/*.gpkg")
 
-out_file = "/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/pot_gmw_chng_ocean_chg_rgns_vecs.gpkg"
+out_file = "/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/pot_gmw_chng_ocean_chg_rgns_nasa_gmwv2_updates_vecs.gpkg"
 
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "pot_gmw_chng_ocean_chg_rgns_vecs", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "pot_gmw_chng_ocean_chg_rgns_nasa_gmwv2_updates_vecs", False)
 
 
