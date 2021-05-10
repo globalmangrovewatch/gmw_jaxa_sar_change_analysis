076a9537a8ac161850431ad9e1373481f2c50865
Pete Bunting <petebunting@mac.com>
2021-05-10 22:52:41 +0100

    Add scripts for creating 2010 water mask.

---
+++ 04_define_gmw_change_regions/09_define_2010_water_mask/create_img_tile.py
@@ -0,0 +1,95 @@
+from pbprocesstools.pbpt_q_process import PBPTQProcessTool
+import logging
+import os
+import shutil
+import rsgislib
+import rsgislib.imagecalc
+import rsgislib.imageutils
+import rsgislib.rastergis
+
+logger = logging.getLogger(__name__)
+
+
+class CreateImageTile(PBPTQProcessTool):
+
+    def __init__(self):
+        super().__init__(cmd_name='create_img_tile.py', descript=None)
+
+    def do_processing(self, **kwargs):
+        if not os.path.exists(self.params['tmp_dir']):
+            os.mkdir(self.params['tmp_dir'])
+
+        if self.params['sar_img'] is None:
+            rsgislib.imagecalc.imageMath(self.params['water_occur_img'], self.params['out_img'], '(b1>80)&&(b1<=100)?1:0', 'KEA', rsgislib.TYPE_8UINT)
+            rsgislib.rastergis.populateStats(self.params['out_img'], addclrtab=True, calcpyramids=True, ignorezero=True)
+        else:
+            per_water_msk_img = os.path.join(self.params['tmp_dir'], '{}_perm_water.kea'.format(self.params['tile']))
+            rsgislib.imagecalc.imageMath(self.params['water_occur_img'], per_water_msk_img, '(b1>90)&&(b1<=100)?1:0', 'KEA', rsgislib.TYPE_8UINT)
+            rsgislib.rastergis.populateStats(per_water_msk_img, addclrtab=True, calcpyramids=False, ignorezero=True)
+
+            n_pxl_vals = rsgislib.imagecalc.countPxlsOfVal(per_water_msk_img, vals=[1])
+            if n_pxl_vals > 0:
+                per_water_sar_img = os.path.join(self.params['tmp_dir'], '{}_perm_water_sar.kea'.format(self.params['tile']))
+                rsgislib.imageutils.maskImage(self.params['sar_img'], per_water_msk_img, per_water_sar_img, 'KEA', rsgislib.TYPE_16INT, 30000, 0)
+
+                sar_water_percent = rsgislib.imagecalc.bandPercentile(per_water_sar_img, 0.99, 30000)
+
+                sar_hh_water_thres = sar_water_percent[0]
+                sar_hv_water_thres = sar_water_percent[1]
+
+            else:
+                per_water_msk_img = os.path.join(self.params['tmp_dir'], '{}_perm_water.kea'.format(self.params['tile']))
+                rsgislib.imagecalc.imageMath(self.params['water_occur_img'], per_water_msk_img, '(b1>70)&&(b1<=100)?1:0', 'KEA', rsgislib.TYPE_8UINT)
+                rsgislib.rastergis.populateStats(per_water_msk_img, addclrtab=True, calcpyramids=False, ignorezero=True)
+
+                n_pxl_vals = rsgislib.imagecalc.countPxlsOfVal(per_water_msk_img, vals=[1])
+                if n_pxl_vals > 0:
+                    per_water_sar_img = os.path.join(self.params['tmp_dir'], '{}_perm_water_sar.kea'.format(self.params['tile']))
+                    rsgislib.imageutils.maskImage(self.params['sar_img'], per_water_msk_img, per_water_sar_img, 'KEA', rsgislib.TYPE_16INT, 30000, 0)
+
+                    sar_water_percent = rsgislib.imagecalc.bandPercentile(per_water_sar_img, 0.99, 30000)
+
+                    sar_hh_water_thres = sar_water_percent[0]
+                    sar_hv_water_thres = sar_water_percent[1]
+                else:
+                    sar_hh_water_thres = -1600
+                    sar_hv_water_thres = -1900
+
+            band_defns = [rsgislib.imagecalc.BandDefn('woccur', self.params['water_occur_img'], 1),
+                          rsgislib.imagecalc.BandDefn('hh', self.params['sar_img'], 1),
+                          rsgislib.imagecalc.BandDefn('hv', self.params['sar_img'], 2)]
+            exp = '(woccur > 5) && (hh > {}) && (hv > {})?1:0'.format(sar_hh_water_thres, sar_hv_water_thres)
+            rsgislib.imagecalc.bandMath(self.params['out_img'], exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+            rsgislib.rastergis.populateStats(self.params['out_img'], addclrtab=True, calcpyramids=True, ignorezero=True)
+
+
+
+
+
+        if os.path.exists(self.params['tmp_dir']):
+            shutil.rmtree(self.params['tmp_dir'])
+
+
+    def required_fields(self, **kwargs):
+        return ["tile", "water_occur_img", "sar_img", "out_img", "tmp_dir"]
+
+
+    def outputs_present(self, **kwargs):
+        files_dict = dict()
+        files_dict[self.params['out_img']] = 'gdal_image'
+        return self.check_files(files_dict)
+
+    def remove_outputs(self, **kwargs):
+        # Remove the output files.
+        if os.path.exists(self.params['out_img']):
+            os.remove(self.params['out_img'])
+
+        # Reset the tmp dir
+        if os.path.exists(self.params['tmp_dir']):
+            shutil.rmtree(self.params['tmp_dir'])
+        os.mkdir(self.params['tmp_dir'])
+
+if __name__ == "__main__":
+    CreateImageTile().std_run()
+
+
---
+++ 04_define_gmw_change_regions/09_define_2010_water_mask/gen_cmds.py
@@ -0,0 +1,62 @@
+from pbprocesstools.pbpt_q_process import PBPTGenQProcessToolCmds
+import logging
+import os
+import glob
+import pathlib
+
+logger = logging.getLogger(__name__)
+
+class GenCmds(PBPTGenQProcessToolCmds):
+
+    def gen_command_info(self, **kwargs):
+        img_tiles = glob.glob(kwargs['gmw_tiles'])
+        for gmw_tile in img_tiles:
+            basename = self.get_file_basename(gmw_tile)
+            tile_basename = self.get_file_basename(gmw_tile, n_comps=2)
+            tile_name = tile_basename.split('_')[1]
+
+            sar_scn_dir = os.path.join(kwargs['sar_tiles_dir'], tile_name)
+
+            water_occur_img = os.path.join(kwargs['water_occur_dir'], '{}_water_occur.kea'.format(tile_basename))
+            sar_img = os.path.join(sar_scn_dir, '{}_2010_db.kea'.format(tile_name))
+            if not os.path.exists(sar_img):
+                sar_img = None
+
+            out_img = os.path.join(kwargs['out_dir'], '{}_2010_water_mask.kea'.format(tile_basename))
+
+            if (not os.path.exists(out_img)):
+                c_dict = dict()
+                c_dict['tile'] = tile_basename
+                c_dict['water_occur_img'] = water_occur_img
+                c_dict['sar_img'] = sar_img
+                c_dict['out_img'] = out_img
+                c_dict['tmp_dir'] = os.path.join(kwargs['tmp_dir'], "{}_2010_water_mask".format(tile_basename))
+                if not os.path.exists(c_dict['tmp_dir']):
+                    os.mkdir(c_dict['tmp_dir'])
+                self.params.append(c_dict)
+
+
+    def run_gen_commands(self):
+        self.gen_command_info(gmw_tiles='/scratch/a.pfb/gmw_v2_gapfill/data/gmw_tiles/gmw_init_v3_qa/*.kea',
+                              sar_tiles_dir='/scratch/a.pfb/gmw_v3_change/data/jaxa_tiles/2010',
+                              water_occur_dir='/scratch/a.pfb/gmw_v3_change/data/other_base_data/water_occurance_gmw_tiles',
+                              out_dir='/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/pot_gmw_chng_ocean_chg_rgns_nasa_gmwv2_updates',
+                              tmp_dir='/scratch/a.pfb/gmw_v3_change/tmp')
+        self.pop_params_db()
+        self.create_slurm_sub_sh("gmw_2010_water_msk", 16448, '/scratch/a.pfb/gmw_v3_change/logs',
+                                 run_script='run_exe_analysis.sh', job_dir="job_scripts",
+                                 db_info_file=None, account_name='scw1376', n_cores_per_job=10, n_jobs=10,
+                                 job_time_limit='2-23:59',
+                                 module_load='module load parallel singularity\n\nexport http_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\nexport https_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\n')
+
+if __name__ == "__main__":
+    py_script = os.path.abspath("create_img_tile.py")
+    script_cmd = "singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python {}".format(py_script)
+
+    process_tools_mod = 'create_img_tile'
+    process_tools_cls = 'CreateImageTile'
+
+    create_tools = GenCmds(cmd=script_cmd, db_conn_file="/home/a.pfb/gmw_gap_fill_db/pbpt_db_conn.txt",
+                                         lock_file_path="./gmw_gapfill_lock_file.txt",
+                                         process_tools_mod=process_tools_mod, process_tools_cls=process_tools_cls)
+    create_tools.parse_cmds()
