ba5e14e68e6c33c74deaede1c968b52fe0c66be6
Pete Bunting <petebunting@mac.com>
2021-05-11 19:06:42 +0100

    Add scripts to create the region used for the low backscatter stats to identify a change threshold.

---
+++ 05_define_2010_baseline/03_non_mang_chng_stats_rgns/gen_cmds.py
@@ -0,0 +1,57 @@
+from pbprocesstools.pbpt_q_process import PBPTGenQProcessToolCmds
+import logging
+import os
+import glob
+import pathlib
+
+logger = logging.getLogger(__name__)
+
+class GenCmds(PBPTGenQProcessToolCmds):
+
+    def gen_command_info(self, **kwargs):
+        img_tiles = glob.glob(kwargs['gmw_tiles'])
+        for gmw_tile in img_tiles:
+            basename = self.get_file_basename(gmw_tile)
+            tile_basename = self.get_file_basename(gmw_tile, n_comps=2)
+
+            water_msk_img = os.path.join(kwargs['water_msk_dir'], '{}_2010_water_mask.kea'.format(tile_basename))
+            gmw_dist_img = os.path.join(kwargs['gmw_v3_init_dist_dir'], '{}_dist_pxls.kea'.format(basename))
+
+            out_img = os.path.join(kwargs['out_dir'], '{}_2010_v3_chg_rgn.kea'.format(tile_basename))
+
+            if (not os.path.exists(out_img)):
+                c_dict = dict()
+                c_dict['tile'] = tile_basename
+                c_dict['water_msk_img'] = water_msk_img
+                c_dict['gmw_dist_img'] = gmw_dist_img
+                c_dict['out_img'] = out_img
+                c_dict['tmp_dir'] = os.path.join(kwargs['tmp_dir'], "{}_2010_v3_chg_rng".format(tile_basename))
+                if not os.path.exists(c_dict['tmp_dir']):
+                    os.mkdir(c_dict['tmp_dir'])
+                self.params.append(c_dict)
+
+
+    def run_gen_commands(self):
+        self.gen_command_info(gmw_tiles='/scratch/a.pfb/gmw_v2_gapfill/data/gmw_tiles/gmw_init_v3_qa/*.kea',
+                              water_msk_dir='/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_2010_init_water_mask',
+                              gmw_v3_init_dist_dir='/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_v3_init_dist',
+                              out_dir='/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_2010_fnl_potent_stats_rgn',
+                              tmp_dir='/scratch/a.pfb/gmw_v3_change/tmp')
+        self.pop_params_db()
+        self.create_slurm_sub_sh("2010_v3_chg_rgn", 16448, '/scratch/a.pfb/gmw_v3_change/logs',
+                                 run_script='run_exe_analysis.sh', job_dir="job_scripts",
+                                 db_info_file=None, account_name='scw1376', n_cores_per_job=10, n_jobs=10,
+                                 job_time_limit='2-23:59',
+                                 module_load='module load parallel singularity\n\nexport http_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\nexport https_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\n')
+
+if __name__ == "__main__":
+    py_script = os.path.abspath("create_img_tile.py")
+    script_cmd = "singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python {}".format(py_script)
+
+    process_tools_mod = 'create_img_tile'
+    process_tools_cls = 'CreateImageTile'
+
+    create_tools = GenCmds(cmd=script_cmd, db_conn_file="/home/a.pfb/gmw_gap_fill_db/pbpt_db_conn.txt",
+                                         lock_file_path="./gmw_gapfill_lock_file.txt",
+                                         process_tools_mod=process_tools_mod, process_tools_cls=process_tools_cls)
+    create_tools.parse_cmds()
