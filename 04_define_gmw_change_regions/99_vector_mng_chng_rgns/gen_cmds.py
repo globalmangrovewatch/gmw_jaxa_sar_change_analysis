33866a6d64fb741a01727b938670dc739948f80f
Pete Bunting <petebunting@mac.com>
2021-05-11 08:01:14 +0100

    Update to create water mask vector

--- 04_define_gmw_change_regions/99_vector_mng_chng_rgns/gen_cmds.py
+++ 04_define_gmw_change_regions/99_vector_mng_chng_rgns/gen_cmds.py
@@ -25,10 +25,10 @@ class GenCmds(PBPTGenQProcessToolCmds):
 
 
     def run_gen_commands(self):
-        self.gen_command_info(tiles_path='/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/pot_gmw_chng_ocean_chg_rgns_nasa_gmwv2_updates/*.kea',
-                              tiles_out_vec='/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/pot_gmw_chng_ocean_chg_rgns_nasa_gmwv2_updates_vecs')
+        self.gen_command_info(tiles_path='/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_2010_init_water_mask/*.kea',
+                              tiles_out_vec='/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_2010_init_water_mask_vecs')
         self.pop_params_db()
-        self.create_slurm_sub_sh("vectorise_mng_potent_chng_rgns", 16448, '/scratch/a.pfb/gmw_v3_change/logs',
+        self.create_slurm_sub_sh("gmw_2010_init_water_mask_vecs", 16448, '/scratch/a.pfb/gmw_v3_change/logs',
                                  run_script='run_exe_analysis.sh', job_dir="job_scripts",
                                  db_info_file=None, account_name='scw1376', n_cores_per_job=10, n_jobs=10,
                                  job_time_limit='2-23:59',
--- 04_define_gmw_change_regions/99_vector_mng_chng_rgns/merge_vecs.py
+++ 04_define_gmw_change_regions/99_vector_mng_chng_rgns/merge_vecs.py
@@ -1,10 +1,10 @@
 import glob
 import rsgislib.vectorutils
 
-input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/pot_gmw_chng_ocean_chg_rgns_nasa_gmwv2_updates_vecs/*.gpkg")
+input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_2010_init_water_mask_vecs/*.gpkg")
 
-out_file = "/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/pot_gmw_chng_ocean_chg_rgns_nasa_gmwv2_updates_vecs.gpkg"
+out_file = "/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_2010_init_water_mask_vecs.gpkg"
 
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "pot_gmw_chng_ocean_chg_rgns_nasa_gmwv2_updates_vecs", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "gmw_2010_init_water_mask_vecs", False)
 
 
