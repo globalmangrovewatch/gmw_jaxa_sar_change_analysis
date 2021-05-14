5466736fa49b5691ce22cc1c8ce4b44f91cae347
Pete Bunting <petebunting@mac.com>
2021-05-14 10:29:45 +0100

    Update to calculate thresholds - not complete yet.

--- 06_find_changes/01_2010_to_2009/02_calc_project_thresholds/calc_project_thresholds.py
+++ 06_find_changes/01_2010_to_2009/02_calc_project_thresholds/calc_project_thresholds.py
@@ -1,6 +1,7 @@
 from pbprocesstools.pbpt_q_process import PBPTQProcessTool
 import logging
 import os
+import shutil
 import rsgislib
 import rsgislib.imageutils
 
@@ -12,6 +13,9 @@ class CalcProjectThreholds(PBPTQProcessTool):
         super().__init__(cmd_name='calc_project_thresholds.py', descript=None)
 
     def do_processing(self, **kwargs):
+
+        
+
         if self.params['sar_img'] is not None:
             rsgis_utils = rsgislib.RSGISPyUtils()
             n_bands = rsgis_utils.getImageBandCount(self.params['sar_img'])
@@ -25,22 +29,22 @@ class CalcProjectThreholds(PBPTQProcessTool):
 
 
     def required_fields(self, **kwargs):
-        return ["tile", "gmw_tile", "potent_chng_msk_img", "sar_img", "out_mng_data", "out_nmng_data"]
-
+        return ["gmw_prj", "mng_data_files", "nmng_data_file", "out_file", "tmp_dir"]
 
     def outputs_present(self, **kwargs):
         files_dict = dict()
-        files_dict[self.params['out_mng_data']] = 'hdf5'
-        files_dict[self.params['out_nmng_data']] = 'hdf5'
+        files_dict[self.params['out_file']] = 'file'
         return self.check_files(files_dict)
 
     def remove_outputs(self, **kwargs):
         # Remove the output files.
-        if os.path.exists(self.params['out_mng_data']):
-            os.remove(self.params['out_mng_data'])
+        if os.path.exists(self.params['out_file']):
+            os.remove(self.params['out_file'])
 
-        if os.path.exists(self.params['out_nmng_data']):
-            os.remove(self.params['out_nmng_data'])
+            # Reset the tmp dir
+            if os.path.exists(self.params['tmp_dir']):
+                shutil.rmtree(self.params['tmp_dir'])
+            os.mkdir(self.params['tmp_dir'])
 
 if __name__ == "__main__":
     CalcProjectThreholds().std_run()
