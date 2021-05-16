de834a74c214ef3a436df2f709d746bfde5b2862
Pete Bunting <petebunting@mac.com>
2021-05-16 22:17:09 +0100

    Add scripts to create a vector output for change year.

---
+++ 06_find_changes/01_2010_to_2009/05_gen_vec_layers/merge_vecs.py
@@ -0,0 +1,13 @@
+import glob
+import rsgislib.vectorutils
+
+out_file = "/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_2010_init_water_mask_vecs.gpkg"
+
+input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_init_2009_v3_vecs/*_v3_init.gpkg")
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "gmw_2009_v3_init", False)
+
+input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_init_2009_v3_vecs/*2009_mng_chng.gpkg")
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "mng_2010_2009_loss", True)
+
+input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_init_2009_v3_vecs/*2009_not_mng_chng.gpkg")
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "mng_2010_2009_gain", True)
