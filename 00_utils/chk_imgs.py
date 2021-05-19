185d5179b13eeec3d5668c9cc2f9972d20e4aa4c
Pete Bunting <petebunting@mac.com>
2021-05-19 22:44:31 +0100

    Update to check number of bands is correct.

--- 00_utils/chk_imgs.py
+++ 00_utils/chk_imgs.py
@@ -5,35 +5,45 @@ import glob
 import numpy
 import h5py
 
-def check_h5_var(h5_obj):
-    OK = True
-    try:
-        if isinstance(h5_obj, h5py.Dataset):
-            OK = True
-        elif isinstance(h5_obj, h5py.Group):
-            for var in h5_obj.keys():
-                OK = check_h5_var(h5_obj[var])
-                if not OK:
-                    break
-    except RuntimeError:
-        OK = False
-    return OK
 
 
-def checkh5(input_file):
-    OK = True
+
+def check_hdf5_file(input_file):
+    """
+    A function which checks whether a HDF5 file is valid.
+    :param input_file: the file path to the input file.
+    :return: a boolean - True file is valid. False file is not valid.
+
+    """
+    import h5py
+
+    def _check_h5_var(h5_obj):
+        lcl_ok = True
+        try:
+            if isinstance(h5_obj, h5py.Dataset):
+                lcl_ok = True
+            elif isinstance(h5_obj, h5py.Group):
+                for var in h5_obj.keys():
+                    lcl_ok = _check_h5_var(h5_obj[var])
+                    if not lcl_ok:
+                        break
+        except RuntimeError:
+            lcl_ok = False
+        return lcl_ok
+    
+    glb_ok = True
     if not os.path.exists(input_file):
-        OK = False
+        glb_ok = False
     else:
         fH5 = h5py.File(input_file, 'r')
         if fH5 is None:
-            OK = False
+            glb_ok = False
         else:
             for var in fH5.keys():
-                OK = check_h5_var(fH5[var])
-                if not OK:
+                glb_ok = _check_h5_var(fH5[var])
+                if not glb_ok:
                     break
-    return OK
+    return glb_ok
 
 
 class RSGISGDALErrorHandler(object):
@@ -67,14 +77,15 @@ class RSGISGDALErrorHandler(object):
         self.err_msg = err_msg
 
 
-def check_gdal_image_file(gdal_img, check_bands=True):
+def check_gdal_image_file(gdal_img, check_bands=True, nbands=0):
     """
     A function which checks a GDAL compatible image file and returns an error message if appropriate.
 
     :param gdal_img: the file path to the gdal image file.
     :param check_bands: boolean specifying whether individual image bands should be
                         opened and checked (Default: True)
-    :return: boolean (True: file OK; False: Error found), string (error message if required otherwise empty string)
+    :param nbands: int specifying the number of expected image bands. Ignored if 0; Default is 0.
+    :return: boolean (True: file ok; False: Error found), string (error message if required otherwise empty string)
 
     """
     file_ok = True
@@ -85,22 +96,30 @@ def check_gdal_image_file(gdal_img, check_bands=True):
         gdal.PushErrorHandler(err_handler)
         gdal.UseExceptions()
         try:
-            if os.path.splitext(gdal_img)[1] == '.kea':
-                file_ok = checkh5(gdal_img)
-                err_str = "Error with HDF5 file."
+            if os.path.splitext(gdal_img)[1].lower() == '.kea':
+                file_ok = check_hdf5_file(gdal_img)
+                err_str = "Error with KEA/HDF5 file."
             if file_ok:
                 raster_ds = gdal.Open(gdal_img, gdal.GA_ReadOnly)
                 if raster_ds is None:
                     file_ok = False
                     err_str = 'GDAL could not open the dataset, returned None.'
-                elif check_bands:
-                    n_bands = raster_ds.RasterCount
-                    for n in range(n_bands):
+
+                if file_ok and (nbands > 0):
+                    n_img_bands = raster_ds.RasterCount
+                    if n_img_bands != nbands:
+                        file_ok = False
+                        err_str = 'Image should have {} image bands but {} found.'.format(nbands, n_img_bands)
+
+                if file_ok and check_bands:
+                    n_img_bands = raster_ds.RasterCount
+                    for n in range(n_img_bands):
                         band = n + 1
                         img_band = raster_ds.GetRasterBand(band)
                         if img_band is None:
                             file_ok = False
                             err_str = 'GDAL could not open band {} in the dataset, returned None.'.format(band)
+                            break
                 raster_ds = None
         except Exception as e:
             file_ok = False
@@ -124,6 +143,7 @@ if __name__ == "__main__":
     parser.add_argument("--size", type=int, default=0, help="Check file sizes - remove lowest X percent")
     parser.add_argument("--rmerr", action='store_true', default=False, help="Delete error files from system.")
     parser.add_argument("--printnames", action='store_true', default=False, help="Print file names as checking")
+    parser.add_argument("--nbands", type=int, default=0, help="Check the number of bands is correct. Ignored if 0; Default.")
 
     args = parser.parse_args()
     print(args.input)
@@ -139,7 +159,7 @@ if __name__ == "__main__":
         print(low_thres)
 
         print("Lowest {} percent file size:".format(args.size))
-        imgs_ok = list()
+        imgs_glb_ok = list()
         for img in imgs:
             if os.path.getsize(img) < low_thres:
                 if args.rmerr:
@@ -148,8 +168,8 @@ if __name__ == "__main__":
                 else:
                     print("rm {}".format(img))
             else:
-                imgs_ok.append(img)
-        imgs = imgs_ok
+                imgs_glb_ok.append(img)
+        imgs = imgs_glb_ok
 
 
     print("File Checks:")
@@ -157,7 +177,7 @@ if __name__ == "__main__":
         if args.printnames:
             print(img)
         try:
-            file_ok, err_str = check_gdal_image_file(img, check_bands=True)
+            file_ok, err_str = check_gdal_image_file(img, check_bands=True, nbands=args.nbands)
             if not file_ok:
                 if args.rmerr:
                     os.remove(img)
