fe1dd92647f66f68f4f8ff3e2467b57709e99839
Pete Bunting <petebunting@mac.com>
2021-05-19 08:13:26 +0100

    Update paths to merge vectors

--- 04_define_gmw_change_regions/99_vector_mng_chng_rgns/merge_vecs.py
+++ 04_define_gmw_change_regions/99_vector_mng_chng_rgns/merge_vecs.py
@@ -1,10 +1,10 @@
 import glob
 import rsgislib.vectorutils
 
-input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/other_base_data/gmw_v2_chng_from_2010_vecs/*.gpkg")
+input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/other_base_data/gmw_v2_chng_from_2010_buf_vecs/*.gpkg")
 
-out_file = "/scratch/a.pfb/gmw_v3_change/data/other_base_data/gmw_v2_chng_from_2010_vecs.gpkg"
+out_file = "/scratch/a.pfb/gmw_v3_change/data/other_base_data/gmw_v2_chng_from_2010_buf_vecs.gpkg"
 
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "gmw_v2_chng_from_2010_vecs", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "gmw_v2_chng_from_2010_buf_vecs", False)
 
 
