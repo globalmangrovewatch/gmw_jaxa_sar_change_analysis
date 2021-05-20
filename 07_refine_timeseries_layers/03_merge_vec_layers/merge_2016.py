336f8e05be39484d642f56775130216edac2f7ac
Pete Bunting <petebunting@mac.com>
2021-05-20 15:52:13 +0100

    Add scripts to create vector layers.

---
+++ 07_refine_timeseries_layers/03_merge_vec_layers/merge_2016.py
@@ -0,0 +1,11 @@
+import glob
+import rsgislib.vectorutils
+
+out_file = "/scratch/a.pfb/gmw_v3_change/data/gmw_v3_exts/gmw_2016_v3.gpkg"
+
+input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2016_v3_fnl_vecs/*.gpkg")
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "gmw_2016_v3", False)
+
+
+
+
