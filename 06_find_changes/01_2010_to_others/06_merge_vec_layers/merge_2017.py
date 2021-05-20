011515d3a299a04450806f98a9eb4f538b085299
Pete Bunting <petebunting@mac.com>
2021-05-20 10:40:28 +0100

    Update to make sure the correct files are selected

--- 06_find_changes/01_2010_to_others/06_merge_vec_layers/merge_2017.py
+++ 06_find_changes/01_2010_to_others/06_merge_vec_layers/merge_2017.py
@@ -6,10 +6,10 @@ out_file = "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_init_2017_v3_vec
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_init_2017_v3_vecs/*_v3_init.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "gmw_init_2017_v3", False)
 
-input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_init_2017_v3_vecs/*_mng_chng.gpkg")
+input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_init_2017_v3_vecs/*2017_mng_chng.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "gmw_2017_2010_loss", True)
 
-input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_init_2017_v3_vecs/*_not_mng_chng.gpkg")
+input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_init_2017_v3_vecs/*2017_not_mng_chng.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "gmw_2017_2010_gain", True)
 
 
