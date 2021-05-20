0ea30eb73535e8d51dc6dcb5f3f9b6d58da62d63
Pete Bunting <petebunting@mac.com>
2021-05-20 09:26:44 +0100

    Update the file name

--- 06_find_changes/01_2010_to_others/06_merge_vec_layers/merge_2015.py
+++ 06_find_changes/01_2010_to_others/06_merge_vec_layers/merge_2015.py
@@ -1,10 +1,10 @@
 import glob
 import rsgislib.vectorutils
 
-out_file = "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2015_v3_vecs.gpkg"
+out_file = "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_init_2015_v3_vecs.gpkg"
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_init_2015_v3_vecs/*_v3_init.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "gmw_2015_v3_vecs", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "gmw_init_2015_v3", False)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_init_2015_v3_vecs/*_mng_chng.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "gmw_2015_2010_loss", True)
