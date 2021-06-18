62d44e7a5f9312947965ef7a615f9ec25ecf5f37
Pete Bunting <petebunting@mac.com>
2021-06-18 11:03:42 +0100

    merge into the same GPKG

--- 05_define_2010_baseline/04_create_vec_chk_files/merge_vecs.py
+++ 05_define_2010_baseline/04_create_vec_chk_files/merge_vecs.py
@@ -7,12 +7,12 @@ rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "gmw_2010_v3", Fals
 
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_2010_fnl_potent_chg_rgn_vecs/*.gpkg")
-out_file = "/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_2010_fnl_potent_chg_rgn_vecs.gpkg"
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "gmw_2010_potent_chng", False)
+#out_file = "/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_2010_fnl_potent_chg_rgn_vecs.gpkg"
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "gmw_2010_potent_chng", True)
 
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_2010_fnl_potent_stats_rgn_vecs/*.gpkg")
-out_file = "/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_2010_fnl_potent_stats_rgn_vecs.gpkg"
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "gmw_2010_potent_chng_stats_rgns", False)
+#out_file = "/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_2010_fnl_potent_stats_rgn_vecs.gpkg"
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "gmw_2010_potent_chng_stats_rgns", True)
 
 
