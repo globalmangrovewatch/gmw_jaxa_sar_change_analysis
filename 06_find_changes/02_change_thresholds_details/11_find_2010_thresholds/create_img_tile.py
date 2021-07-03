8c6dc7131e073de3dc7dda93e3422f024d81c95b
Pete Bunting <petebunting@mac.com>
2021-07-03 11:27:43 +0100

    Run analysis to find where the HV thresholds for the 2010 data is identified.

---
+++ 06_find_changes/02_change_thresholds_details/11_find_2010_thresholds/create_img_tile.py
@@ -0,0 +1,319 @@
+from pbprocesstools.pbpt_q_process import PBPTQProcessTool
+import logging
+import os
+import shutil
+import pprint
+import rsgislib
+import rsgislib.rastergis
+import rsgislib.imagecalc
+import rsgislib.imageutils
+import numpy
+
+def create_1996_mng_msk(gmw_tile, sar_img, sar_vld_img, threshold, out_img, out_uncertain_img, tmp_dir, thres_var=200, n_steps=10):
+
+    step_intvl = thres_var / int(n_steps/2)
+    threshold_steps = numpy.arange(1, int(n_steps/2)+1) * step_intvl
+    rsgis_utils = rsgislib.RSGISPyUtils()
+    basename = rsgis_utils.get_file_basename(out_img)
+
+    imgs = list()
+
+    # Using threshold
+    band_defns = []
+    band_defns.append(rsgislib.imagecalc.BandDefn('gmw', gmw_tile, 1))
+    band_defns.append(rsgislib.imagecalc.BandDefn('hh', sar_img, 1))
+    band_defns.append(rsgislib.imagecalc.BandDefn('vld', sar_vld_img, 1))
+    exp = '(vld==1)&&(gmw==1)&&(hh<{})?1:0'.format(threshold)
+    rsgislib.imagecalc.bandMath(out_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+    rsgislib.rastergis.populateStats(out_img, True, True, True)
+    imgs.append(out_img)
+
+    # Add intervals
+    i = 1
+    for step in threshold_steps:
+        out_tmp_img = os.path.join(tmp_dir, "{}_img_{}.kea".format(basename, i))
+        band_defns = []
+        band_defns.append(rsgislib.imagecalc.BandDefn('gmw', gmw_tile, 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('hh', sar_img, 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('vld', sar_vld_img, 1))
+        exp = '(vld==1)&&(gmw==1)&&(hh<{})?1:0'.format(threshold+step)
+        rsgislib.imagecalc.bandMath(out_tmp_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+        imgs.append(out_tmp_img)
+        i += 1
+
+    # Remove intervals
+    for step in threshold_steps:
+        out_tmp_img = os.path.join(tmp_dir, "{}_img_{}.kea".format(basename, i))
+        band_defns = []
+        band_defns.append(rsgislib.imagecalc.BandDefn('gmw', gmw_tile, 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('hh', sar_img, 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('vld', sar_vld_img, 1))
+        exp = '(vld==1)&&(gmw==1)&&(hh<{})?1:0'.format(threshold-step)
+        rsgislib.imagecalc.bandMath(out_tmp_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+        imgs.append(out_tmp_img)
+        i += 1
+
+    rsgislib.imagecalc.calcMultiImgBandStats(imgs, out_uncertain_img, rsgislib.SUMTYPE_SUM, 'KEA', rsgislib.TYPE_8UINT, 0, False)
+    rsgislib.imageutils.popImageStats(out_uncertain_img, usenodataval=True, nodataval=0, calcpyramids=True)
+
+def create_1996_nmng_msk(chng_rgn_tile, sar_img, sar_vld_img, threshold, out_img, out_uncertain_img, tmp_dir, thres_var=200, n_steps=10):
+    step_intvl = thres_var / int(n_steps / 2)
+    threshold_steps = numpy.arange(1, int(n_steps / 2)+1) * step_intvl
+    rsgis_utils = rsgislib.RSGISPyUtils()
+    basename = rsgis_utils.get_file_basename(out_img)
+
+    imgs = list()
+
+    # Using threshold
+    band_defns = []
+    band_defns.append(rsgislib.imagecalc.BandDefn('chgmsk', chng_rgn_tile, 1))
+    band_defns.append(rsgislib.imagecalc.BandDefn('hh', sar_img, 1))
+    band_defns.append(rsgislib.imagecalc.BandDefn('vld', sar_vld_img, 1))
+    exp = '(vld==1)&&(chgmsk==1)&&(hh>{})?1:0'.format(threshold)
+    rsgislib.imagecalc.bandMath(out_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+    rsgislib.rastergis.populateStats(out_img, True, True, True)
+    imgs.append(out_img)
+
+    # Add intervals
+    i = 1
+    for step in threshold_steps:
+        out_tmp_img = os.path.join(tmp_dir, "{}_img_{}.kea".format(basename, i))
+        band_defns = []
+        band_defns.append(rsgislib.imagecalc.BandDefn('chgmsk', chng_rgn_tile, 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('hh', sar_img, 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('vld', sar_vld_img, 1))
+        exp = '(vld==1)&&(chgmsk==1)&&(hh>{})?1:0'.format(threshold + step)
+        rsgislib.imagecalc.bandMath(out_tmp_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+        imgs.append(out_tmp_img)
+        i += 1
+
+    # Remove intervals
+    for step in threshold_steps:
+        out_tmp_img = os.path.join(tmp_dir, "{}_img_{}.kea".format(basename, i))
+        band_defns = []
+        band_defns.append(rsgislib.imagecalc.BandDefn('chgmsk', chng_rgn_tile, 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('hh', sar_img, 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('vld', sar_vld_img, 1))
+        exp = '(vld==1)&&(chgmsk==1)&&(hh>{})?1:0'.format(threshold - step)
+        rsgislib.imagecalc.bandMath(out_tmp_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+        imgs.append(out_tmp_img)
+        i += 1
+
+    rsgislib.imagecalc.calcMultiImgBandStats(imgs, out_uncertain_img, rsgislib.SUMTYPE_SUM, 'KEA', rsgislib.TYPE_8UINT, 0, False)
+    rsgislib.imageutils.popImageStats(out_uncertain_img, usenodataval=True, nodataval=0, calcpyramids=True)
+
+
+
+
+def create_alos_mng_msk(gmw_tile, sar_img, sar_vld_img, threshold, out_img, out_uncertain_img, tmp_dir, thres_var=200, n_steps=10):
+    step_intvl = thres_var / int(n_steps / 2)
+    threshold_steps = numpy.arange(1, int(n_steps / 2)+1) * step_intvl
+    rsgis_utils = rsgislib.RSGISPyUtils()
+    basename = rsgis_utils.get_file_basename(out_img)
+
+    imgs = list()
+
+    # Using threshold
+    band_defns = []
+    band_defns.append(rsgislib.imagecalc.BandDefn('gmw', gmw_tile, 1))
+    band_defns.append(rsgislib.imagecalc.BandDefn('hv', sar_img, 2))
+    band_defns.append(rsgislib.imagecalc.BandDefn('vld', sar_vld_img, 1))
+    exp = '(vld==1)&&(gmw==1)&&(hv<{})?1:0'.format(threshold)
+    rsgislib.imagecalc.bandMath(out_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+    rsgislib.rastergis.populateStats(out_img, True, True, True)
+    imgs.append(out_img)
+
+    # Add intervals
+    i = 1
+    for step in threshold_steps:
+        out_tmp_img = os.path.join(tmp_dir, "{}_img_{}.kea".format(basename, i))
+        band_defns = []
+        band_defns.append(rsgislib.imagecalc.BandDefn('gmw', gmw_tile, 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('hv', sar_img, 2))
+        band_defns.append(rsgislib.imagecalc.BandDefn('vld', sar_vld_img, 1))
+        exp = '(vld==1)&&(gmw==1)&&(hv<{})?1:0'.format(threshold + step)
+        rsgislib.imagecalc.bandMath(out_tmp_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+        imgs.append(out_tmp_img)
+        i += 1
+
+    # Remove intervals
+    for step in threshold_steps:
+        out_tmp_img = os.path.join(tmp_dir, "{}_img_{}.kea".format(basename, i))
+        band_defns = []
+        band_defns.append(rsgislib.imagecalc.BandDefn('gmw', gmw_tile, 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('hv', sar_img, 2))
+        band_defns.append(rsgislib.imagecalc.BandDefn('vld', sar_vld_img, 1))
+        exp = '(vld==1)&&(gmw==1)&&(hv<{})?1:0'.format(threshold - step)
+        rsgislib.imagecalc.bandMath(out_tmp_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+        imgs.append(out_tmp_img)
+        i += 1
+
+    rsgislib.imagecalc.calcMultiImgBandStats(imgs, out_uncertain_img, rsgislib.SUMTYPE_SUM, 'KEA', rsgislib.TYPE_8UINT, 0, False)
+    rsgislib.imageutils.popImageStats(out_uncertain_img, usenodataval=True, nodataval=0, calcpyramids=True)
+
+
+
+def create_alos_nmng_msk(chng_rgn_tile, sar_img, sar_vld_img, threshold, out_img, out_uncertain_img, tmp_dir, thres_var=200, n_steps=10):
+    step_intvl = thres_var / int(n_steps / 2)
+    threshold_steps = numpy.arange(1, int(n_steps / 2)+1) * step_intvl
+    rsgis_utils = rsgislib.RSGISPyUtils()
+    basename = rsgis_utils.get_file_basename(out_img)
+
+    imgs = list()
+
+    # Using threshold
+    band_defns = []
+    band_defns.append(rsgislib.imagecalc.BandDefn('chgmsk', chng_rgn_tile, 1))
+    band_defns.append(rsgislib.imagecalc.BandDefn('hv', sar_img, 2))
+    band_defns.append(rsgislib.imagecalc.BandDefn('vld', sar_vld_img, 1))
+    exp = '(vld==1)&&(chgmsk==1)&&(hv>{})?1:0'.format(threshold)
+    rsgislib.imagecalc.bandMath(out_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+    rsgislib.rastergis.populateStats(out_img, True, True, True)
+    imgs.append(out_img)
+
+    # Add intervals
+    i = 1
+    for step in threshold_steps:
+        out_tmp_img = os.path.join(tmp_dir, "{}_img_{}.kea".format(basename, i))
+        band_defns = []
+        band_defns.append(rsgislib.imagecalc.BandDefn('chgmsk', chng_rgn_tile, 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('hv', sar_img, 2))
+        band_defns.append(rsgislib.imagecalc.BandDefn('vld', sar_vld_img, 1))
+        exp = '(vld==1)&&(chgmsk==1)&&(hv>{})?1:0'.format(threshold + step)
+        rsgislib.imagecalc.bandMath(out_tmp_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+        imgs.append(out_tmp_img)
+        i += 1
+
+    # Remove intervals
+    for step in threshold_steps:
+        out_tmp_img = os.path.join(tmp_dir, "{}_img_{}.kea".format(basename, i))
+        band_defns = []
+        band_defns.append(rsgislib.imagecalc.BandDefn('chgmsk', chng_rgn_tile, 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('hv', sar_img, 2))
+        band_defns.append(rsgislib.imagecalc.BandDefn('vld', sar_vld_img, 1))
+        exp = '(vld==1)&&(chgmsk==1)&&(hv>{})?1:0'.format(threshold - step)
+        rsgislib.imagecalc.bandMath(out_tmp_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+        imgs.append(out_tmp_img)
+        i += 1
+
+    rsgislib.imagecalc.calcMultiImgBandStats(imgs, out_uncertain_img, rsgislib.SUMTYPE_SUM, 'KEA', rsgislib.TYPE_8UINT, 0, False)
+    rsgislib.imageutils.popImageStats(out_uncertain_img, usenodataval=True, nodataval=0, calcpyramids=True)
+
+logger = logging.getLogger(__name__)
+
+class CreateImageTile(PBPTQProcessTool):
+
+    def __init__(self):
+        super().__init__(cmd_name='create_img_tile.py', descript=None)
+
+    def do_processing(self, **kwargs):
+        if not os.path.exists(self.params['tmp_dir']):
+            os.mkdir(self.params['tmp_dir'])
+
+        rsgis_utils = rsgislib.RSGISPyUtils()
+
+        out_stats = dict()
+        out_stats['gmw_pxls'] = 0
+        out_stats['pochng_pxls'] = 0
+        out_stats['gmw_pxls_gt'] = list()
+        out_stats['pochng_pxls_gt'] = list()
+        out_stats['thresholds_gt'] = list()
+        out_stats['gmw_pxls_lt'] = list()
+        out_stats['pochng_pxls_lt'] = list()
+        out_stats['thresholds_lt'] = list()
+
+        if self.params['sar_img'] is not None:
+            basename = rsgis_utils.get_file_basename(self.params['sar_img'])
+
+            out_tmp_gmw_img = os.path.join(self.params['tmp_dir'], "{}_gmw_pxls.kea".format(basename))
+            band_defns = []
+            band_defns.append(rsgislib.imagecalc.BandDefn('gmw', self.params['gmw_tile'], 1))
+            band_defns.append(rsgislib.imagecalc.BandDefn('vld', self.params['sar_vld_img'], 1))
+            rsgislib.imagecalc.bandMath(out_tmp_gmw_img, '(vld==1)&&(gmw==1)?1:0', 'KEA', rsgislib.TYPE_8UINT, band_defns)
+            out_stats['gmw_pxls'] = int(rsgislib.imagecalc.countPxlsOfVal(out_tmp_gmw_img, vals=[1])[0])
+
+            out_tmp_pochng_img = os.path.join(self.params['tmp_dir'], "{}_pochng_pxls.kea".format(basename))
+            band_defns = []
+            band_defns.append(rsgislib.imagecalc.BandDefn('pochng', self.params['potent_chng_msk_img'], 1))
+            band_defns.append(rsgislib.imagecalc.BandDefn('vld', self.params['sar_vld_img'], 1))
+            rsgislib.imagecalc.bandMath(out_tmp_pochng_img, '(vld==1)&&(pochng==1)?1:0', 'KEA', rsgislib.TYPE_8UINT, band_defns)
+            out_stats['pochng_pxls'] = int(rsgislib.imagecalc.countPxlsOfVal(out_tmp_pochng_img, vals=[1])[0])
+
+            if (out_stats['gmw_pxls'] > 0) or (out_stats['pochng_pxls'] > 0):
+                thresholds = numpy.arange(-3000, -500, 50)
+                for threshold in thresholds:
+                    # Greater than threshold
+                    out_stats['thresholds_gt'].append(threshold)
+                    out_tmp_img = os.path.join(self.params['tmp_dir'], "{}_gt_thres_{}.kea".format(basename, abs(threshold)))
+                    band_defns = []
+                    band_defns.append(rsgislib.imagecalc.BandDefn('hv', self.params['sar_img'], 2))
+                    band_defns.append(rsgislib.imagecalc.BandDefn('vld', self.params['sar_vld_img'], 1))
+                    exp = '(vld==1)&&(hv>{})?1:0'.format(threshold)
+                    rsgislib.imagecalc.bandMath(out_tmp_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+
+                    out_tmp_gmw_img = os.path.join(self.params['tmp_dir'], "{}_gt_thres_{}_GMW.kea".format(basename, abs(threshold)))
+                    band_defns = []
+                    band_defns.append(rsgislib.imagecalc.BandDefn('gmw', self.params['gmw_tile'], 1))
+                    band_defns.append(rsgislib.imagecalc.BandDefn('msk', out_tmp_img, 1))
+                    rsgislib.imagecalc.bandMath(out_tmp_gmw_img, '(msk==1)&&(gmw==1)?1:0', 'KEA', rsgislib.TYPE_8UINT, band_defns)
+                    out_stats['gmw_pxls_gt'].append(int(rsgislib.imagecalc.countPxlsOfVal(out_tmp_gmw_img, vals=[1])[0]))
+
+                    out_tmp_pochng_img = os.path.join(self.params['tmp_dir'], "{}_gt_thres_{}_pochng.kea".format(basename, abs(threshold)))
+                    band_defns = []
+                    band_defns.append(rsgislib.imagecalc.BandDefn('pochng', self.params['potent_chng_msk_img'], 1))
+                    band_defns.append(rsgislib.imagecalc.BandDefn('msk', out_tmp_img, 1))
+                    rsgislib.imagecalc.bandMath(out_tmp_pochng_img, '(msk==1)&&(pochng==1)?1:0', 'KEA', rsgislib.TYPE_8UINT, band_defns)
+                    out_stats['pochng_pxls_gt'].append(int(rsgislib.imagecalc.countPxlsOfVal(out_tmp_pochng_img, vals=[1])[0]))
+
+                    # Less than threshold
+                    out_stats['thresholds_lt'].append(threshold)
+                    out_tmp_img = os.path.join(self.params['tmp_dir'], "{}_lt_thres_{}.kea".format(basename, abs(threshold)))
+                    band_defns = []
+                    band_defns.append(rsgislib.imagecalc.BandDefn('hv', self.params['sar_img'], 2))
+                    band_defns.append(rsgislib.imagecalc.BandDefn('vld', self.params['sar_vld_img'], 1))
+                    exp = '(vld==1)&&(hv<{})?1:0'.format(threshold)
+                    rsgislib.imagecalc.bandMath(out_tmp_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+
+                    out_tmp_gmw_img = os.path.join(self.params['tmp_dir'], "{}_lt_thres_{}_GMW.kea".format(basename, abs(threshold)))
+                    band_defns = []
+                    band_defns.append(rsgislib.imagecalc.BandDefn('gmw', self.params['gmw_tile'], 1))
+                    band_defns.append(rsgislib.imagecalc.BandDefn('msk', out_tmp_img, 1))
+                    rsgislib.imagecalc.bandMath(out_tmp_gmw_img, '(msk==1)&&(gmw==1)?1:0', 'KEA', rsgislib.TYPE_8UINT, band_defns)
+                    out_stats['gmw_pxls_lt'].append(int(rsgislib.imagecalc.countPxlsOfVal(out_tmp_gmw_img, vals=[1])[0]))
+
+                    out_tmp_pochng_img = os.path.join(self.params['tmp_dir'], "{}_lt_thres_{}_pochng.kea".format(basename, abs(threshold)))
+                    band_defns = []
+                    band_defns.append(rsgislib.imagecalc.BandDefn('pochng', self.params['potent_chng_msk_img'], 1))
+                    band_defns.append(rsgislib.imagecalc.BandDefn('msk', out_tmp_img, 1))
+                    rsgislib.imagecalc.bandMath(out_tmp_pochng_img, '(msk==1)&&(pochng==1)?1:0', 'KEA', rsgislib.TYPE_8UINT, band_defns)
+                    out_stats['pochng_pxls_lt'].append(int(rsgislib.imagecalc.countPxlsOfVal(out_tmp_pochng_img, vals=[1])[0]))
+
+        pprint.pprint(out_stats)
+        rsgis_utils.writeDict2JSON(out_stats, self.params['out_file'])
+
+        if os.path.exists(self.params['tmp_dir']):
+            shutil.rmtree(self.params['tmp_dir'])
+
+
+    def required_fields(self, **kwargs):
+        return ["tile", "gmw_tile", "potent_chng_msk_img", "sar_img", "sar_vld_img", "out_file", "tmp_dir"]
+
+    def outputs_present(self, **kwargs):
+        files_dict = dict()
+        files_dict[self.params['out_file']] = 'file'
+        return self.check_files(files_dict)
+
+    def remove_outputs(self, **kwargs):
+        # Remove the output files.
+        if os.path.exists(self.params['out_file']):
+            os.remove(self.params['out_file'])
+
+        # Reset the tmp dir
+        if os.path.exists(self.params['tmp_dir']):
+            shutil.rmtree(self.params['tmp_dir'])
+        os.mkdir(self.params['tmp_dir'])
+
+if __name__ == "__main__":
+    CreateImageTile().std_run()
+
+
---
+++ 06_find_changes/02_change_thresholds_details/11_find_2010_thresholds/gen_cmds.py
@@ -0,0 +1,79 @@
+from pbprocesstools.pbpt_q_process import PBPTGenQProcessToolCmds
+import logging
+import os
+import glob
+import rsgislib
+
+logger = logging.getLogger(__name__)
+
+class GenCmds(PBPTGenQProcessToolCmds):
+
+    def gen_command_info(self, **kwargs):
+        if not os.path.exists(kwargs['out_dir']):
+            os.mkdir(kwargs['out_dir'])
+
+        rsgis_utils = rsgislib.RSGISPyUtils()
+        tile_prj_lut = rsgis_utils.readJSON2Dict(kwargs['prjs_lut_file'])
+
+        img_tiles = glob.glob(kwargs['gmw_tiles'])
+        for gmw_tile in img_tiles:
+            basename = self.get_file_basename(gmw_tile)
+            tile_basename = self.get_file_basename(gmw_tile, n_comps=2)
+            tile_name = tile_basename.split('_')[1]
+
+            sar_scn_dir = os.path.join(kwargs['sar_tiles_dir'], tile_name)
+            if os.path.exists(sar_scn_dir):
+                sar_img = os.path.join(sar_scn_dir, '{}_{}_db.kea'.format(tile_name, kwargs['sar_year']))
+                sar_vld_img = os.path.join(sar_scn_dir, '{}_{}_bin_vmsk.kea'.format(tile_name, kwargs['sar_year']))
+                if not os.path.exists(sar_img):
+                    sar_img = None
+                    sar_vld_img = None
+            else:
+                sar_img = None
+                sar_vld_img = None
+
+            potent_chng_msk_img = os.path.join(kwargs['potent_chng_msk_dir'], '{}_2010_v3_chg_rgn.kea'.format(tile_basename))
+
+
+            out_file = os.path.join(kwargs['out_dir'], '{}_2010_threshold_tests.json'.format(tile_basename))
+
+            if (not os.path.exists(out_file)):
+                c_dict = dict()
+                c_dict['tile'] = tile_basename
+                c_dict['gmw_tile'] = gmw_tile
+                c_dict['potent_chng_msk_img'] = potent_chng_msk_img
+                c_dict['sar_img'] = sar_img
+                c_dict['sar_vld_img'] = sar_vld_img
+                c_dict['out_file'] = out_file
+                c_dict['tmp_dir'] = os.path.join(kwargs['tmp_dir'], "{}_2010_threshold_tests".format(tile_basename))
+                if not os.path.exists(c_dict['tmp_dir']):
+                    os.mkdir(c_dict['tmp_dir'])
+                self.params.append(c_dict)
+
+
+    def run_gen_commands(self):
+
+        self.gen_command_info(gmw_tiles='/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_2010_v3/*.kea',
+                              potent_chng_msk_dir='/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_2010_fnl_potent_chg_rgn',
+                              sar_tiles_dir='/scratch/a.pfb/gmw_v3_change/data/jaxa_tiles/2010',
+                              out_dir='/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_test_thresholds',
+                              tmp_dir='/scratch/a.pfb/gmw_v3_change/tmp')
+
+        self.pop_params_db()
+        self.create_slurm_sub_sh("gmw_2010_test_thresholds", 16448, '/scratch/a.pfb/gmw_v3_change/logs',
+                                 run_script='run_exe_analysis.sh', job_dir="job_scripts",
+                                 db_info_file=None, account_name='scw1376', n_cores_per_job=10, n_jobs=10,
+                                 job_time_limit='2-23:59',
+                                 module_load='module load parallel singularity\n\nexport http_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\nexport https_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\n')
+
+if __name__ == "__main__":
+    py_script = os.path.abspath("create_img_tile.py")
+    script_cmd = "singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python {}".format(py_script)
+
+    process_tools_mod = 'create_img_tile'
+    process_tools_cls = 'CreateImageTile'
+
+    create_tools = GenCmds(cmd=script_cmd, db_conn_file="/home/a.pfb/gmw_gap_fill_db/pbpt_db_conn.txt",
+                                         lock_file_path="./gmw_gapfill_lock_file.txt",
+                                         process_tools_mod=process_tools_mod, process_tools_cls=process_tools_cls)
+    create_tools.parse_cmds()
