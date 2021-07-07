feaa308df5506d30af33e785af26ef8f171f70d2
Pete Bunting <petebunting@mac.com>
2021-07-07 20:06:24 +0100

    Check there is enough data.

--- 06_find_changes/01_2010_to_others/02a_calc_project_thresholds/calc_project_thresholds.py
+++ 06_find_changes/01_2010_to_others/02a_calc_project_thresholds/calc_project_thresholds.py
@@ -84,6 +84,8 @@ def calc_kurt_skew_threshold(data, max_val, min_val, init_thres, low_thres=True,
     if len(data.shape) > 1:
         raise Exception("Expecting a single variable.")
     print("Data Shape: {}".format(data.shape))
+    if data.shape[0] < 100:
+        raise Exception("Not enough data to calculate thresholds.")
     if (contamination < 1) or (contamination > 100):
         raise Exception("contamination parameter should have a value between 1 and 100.")
 
