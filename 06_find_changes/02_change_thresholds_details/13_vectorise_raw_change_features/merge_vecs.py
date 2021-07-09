2b85466ba6250dee8571337558dfeff44efc6047
Pete Bunting <petebunting@mac.com>
2021-07-09 20:50:35 +0100

    Doh... was overwriting the file for each year rather than adding layer.

--- 06_find_changes/02_change_thresholds_details/13_vectorise_raw_change_features/merge_vecs.py
+++ 06_find_changes/02_change_thresholds_details/13_vectorise_raw_change_features/merge_vecs.py
@@ -12,63 +12,63 @@ rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_1996_mng
 
 # 2007
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2007_chngs_vecs/*2007_mng_chng.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2007_nmng", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2007_nmng", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2007_chngs_vecs/*2007_not_mng_chng.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2007_mng", True)
 
 # 2008
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2008_chngs_vecs/*2008_mng_chng.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2008_nmng", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2008_nmng", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2008_chngs_vecs/*2008_not_mng_chng.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2008_mng", True)
 
 # 2009
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2009_chngs_vecs/*2009_mng_chng.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2009_nmng", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2009_nmng", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2009_chngs_vecs/*2009_not_mng_chng.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2009_mng", True)
 
 # 2015
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2015_chngs_vecs/*2015_mng_chng.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2015_nmng", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2015_nmng", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2015_chngs_vecs/*2015_not_mng_chng.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2015_mng", True)
 
 # 2016
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2016_chngs_vecs/*2016_mng_chng.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2016_nmng", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2016_nmng", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2016_chngs_vecs/*2016_not_mng_chng.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2016_mng", True)
 
 # 2017
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2017_chngs_vecs/*2017_mng_chng.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2017_nmng", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2017_nmng", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2017_chngs_vecs/*2017_not_mng_chng.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2017_mng", True)
 
 # 2018
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2018_chngs_vecs/*2018_mng_chng.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2018_nmng", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2018_nmng", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2018_chngs_vecs/*2018_not_mng_chng.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2018_mng", True)
 
 # 2019
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2019_chngs_vecs/*2019_mng_chng.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2019_nmng", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2019_nmng", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2019_chngs_vecs/*2019_not_mng_chng.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2019_mng", True)
 
 # 2020
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2020_chngs_vecs/*2020_mng_chng.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2020_nmng", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2020_nmng", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2020_chngs_vecs/*2020_not_mng_chng.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2020_mng", True)
@@ -87,63 +87,63 @@ rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_1996_mng
 
 # 2007
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2007_chngs_vecs/*2007_mng_chng_upper.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2007_nmng_upper", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2007_nmng_upper", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2007_chngs_vecs/*2007_not_mng_chng_upper.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2007_mng_upper", True)
 
 # 2008
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2008_chngs_vecs/*2008_mng_chng_upper.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2008_nmng_upper", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2008_nmng_upper", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2008_chngs_vecs/*2008_not_mng_chng_upper.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2008_mng_upper", True)
 
 # 2009
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2009_chngs_vecs/*2009_mng_chng_upper.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2009_nmng_upper", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2009_nmng_upper", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2009_chngs_vecs/*2009_not_mng_chng_upper.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2009_mng_upper", True)
 
 # 2015
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2015_chngs_vecs/*2015_mng_chng_upper.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2015_nmng_upper", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2015_nmng_upper", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2015_chngs_vecs/*2015_not_mng_chng_upper.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2015_mng_upper", True)
 
 # 2016
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2016_chngs_vecs/*2016_mng_chng_upper.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2016_nmng_upper", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2016_nmng_upper", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2016_chngs_vecs/*2016_not_mng_chng_upper.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2016_mng_upper", True)
 
 # 2017
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2017_chngs_vecs/*2017_mng_chng_upper.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2017_nmng_upper", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2017_nmng_upper", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2017_chngs_vecs/*2017_not_mng_chng_upper.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2017_mng_upper", True)
 
 # 2018
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2018_chngs_vecs/*2018_mng_chng_upper.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2018_nmng_upper", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2018_nmng_upper", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2018_chngs_vecs/*2018_not_mng_chng_upper.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2018_mng_upper", True)
 
 # 2019
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2019_chngs_vecs/*2019_mng_chng_upper.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2019_nmng_upper", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2019_nmng_upper", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2019_chngs_vecs/*2019_not_mng_chng_upper.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2019_mng_upper", True)
 
 # 2020
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2020_chngs_vecs/*2020_mng_chng_upper.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2020_nmng_upper", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2020_nmng_upper", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2020_chngs_vecs/*2020_not_mng_chng_upper.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2020_mng_upper", True)
@@ -163,63 +163,63 @@ rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_1996_mng
 
 # 2007
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2007_chngs_vecs/*2007_mng_chng_lower.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2007_nmng_lower", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2007_nmng_lower", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2007_chngs_vecs/*2007_not_mng_chng_lower.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2007_mng_lower", True)
 
 # 2008
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2008_chngs_vecs/*2008_mng_chng_lower.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2008_nmng_lower", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2008_nmng_lower", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2008_chngs_vecs/*2008_not_mng_chng_lower.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2008_mng_lower", True)
 
 # 2009
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2009_chngs_vecs/*2009_mng_chng_lower.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2009_nmng_lower", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2009_nmng_lower", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2009_chngs_vecs/*2009_not_mng_chng_lower.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2009_mng_lower", True)
 
 # 2015
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2015_chngs_vecs/*2015_mng_chng_lower.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2015_nmng_lower", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2015_nmng_lower", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2015_chngs_vecs/*2015_not_mng_chng_lower.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2015_mng_lower", True)
 
 # 2016
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2016_chngs_vecs/*2016_mng_chng_lower.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2016_nmng_lower", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2016_nmng_lower", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2016_chngs_vecs/*2016_not_mng_chng_lower.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2016_mng_lower", True)
 
 # 2017
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2017_chngs_vecs/*2017_mng_chng_lower.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2017_nmng_lower", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2017_nmng_lower", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2017_chngs_vecs/*2017_not_mng_chng_lower.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2017_mng_lower", True)
 
 # 2018
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2018_chngs_vecs/*2018_mng_chng_lower.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2018_nmng_lower", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2018_nmng_lower", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2018_chngs_vecs/*2018_not_mng_chng_lower.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2018_mng_lower", True)
 
 # 2019
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2019_chngs_vecs/*2019_mng_chng_lower.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2019_nmng_lower", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2019_nmng_lower", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2019_chngs_vecs/*2019_not_mng_chng_lower.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2019_mng_lower", True)
 
 # 2020
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2020_chngs_vecs/*2020_mng_chng_lower.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2020_nmng_lower", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_mng_2020_nmng_lower", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2020_chngs_vecs/*2020_not_mng_chng_lower.gpkg")
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "2010_nmng_2020_mng_lower", True)
