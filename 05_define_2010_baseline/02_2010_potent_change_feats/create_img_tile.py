78c7b4011324b328c2aa8e1ca3e2735a1b265cbb
Pete Bunting <petebunting@mac.com>
2021-07-10 18:49:19 +0100

    Update the potential change regions using the JRC water change layer.

--- 05_define_2010_baseline/02_2010_potent_change_feats/create_img_tile.py
+++ 05_define_2010_baseline/02_2010_potent_change_feats/create_img_tile.py
@@ -17,15 +17,16 @@ class CreateImageTile(PBPTQProcessTool):
 
     def do_processing(self, **kwargs):
         band_defns = [rsgislib.imagecalc.BandDefn('wmsk', self.params['water_msk_img'], 1),
+                      rsgislib.imagecalc.BandDefn('wchng', self.params['water_chng_img'], 1),
                       rsgislib.imagecalc.BandDefn('pchng', self.params['pot_chng_rgns_img'], 1),
                       rsgislib.imagecalc.BandDefn('gmw', self.params['gmw_tile'], 1),
                       rsgislib.imagecalc.BandDefn('gmwinit', self.params['gmw_init_msk_img'], 1)]
-        rsgislib.imagecalc.bandMath(self.params['out_img'], '(gmwinit==1)&&(gmw==0)?1:(pchng==1)&&(wmsk==1)?1:0', 'KEA', rsgislib.TYPE_8UINT, band_defns)
+        rsgislib.imagecalc.bandMath(self.params['out_img'], '(gmwinit==1)&&(gmw==0)?1:(pchng==1)&&(wmsk==1)&&(wchng!=100)?1:0', 'KEA', rsgislib.TYPE_8UINT, band_defns)
         rsgislib.rastergis.populateStats(self.params['out_img'], addclrtab=True, calcpyramids=True, ignorezero=True)
 
 
     def required_fields(self, **kwargs):
-        return ["tile", "gmw_tile", "gmw_init_msk_img", "water_msk_img", "pot_chng_rgns_img", "out_img"]
+        return ["tile", "gmw_tile", "gmw_init_msk_img", "water_msk_img", "water_chng_img", "pot_chng_rgns_img", "out_img"]
 
 
     def outputs_present(self, **kwargs):
--- 05_define_2010_baseline/02_2010_potent_change_feats/gen_cmds.py
+++ 05_define_2010_baseline/02_2010_potent_change_feats/gen_cmds.py
@@ -16,17 +16,19 @@ class GenCmds(PBPTGenQProcessToolCmds):
 
             gmw_init_msk_img = os.path.join(kwargs['gmw_init_tiles'], '{}_tile_gmw_v3_init_qad.kea'.format(tile_basename))
             water_msk_img = os.path.join(kwargs['water_msk_dir'], '{}_2010_water_mask.kea'.format(tile_basename))
+            water_chng_img = os.path.join(kwargs['water_change_dir'], '{}_water_change.kea'.format(tile_basename))
             pot_chng_rgns_img = os.path.join(kwargs['potent_chg_rgns_dir'], '{}_gmw_v3_pot_chng_ocean_chg_rgns_up_gmwv2_nasa_qad.kea'.format(tile_basename))
 
             out_img = os.path.join(kwargs['out_dir'], '{}_2010_v3_chg_rgn.kea'.format(tile_basename))
 
             if (not os.path.exists(out_img)):
-                print("rm {}".format(gmw_tile))
+                #print("rm {}".format(gmw_tile))
                 c_dict = dict()
                 c_dict['tile'] = tile_basename
                 c_dict['gmw_tile'] = gmw_tile
                 c_dict['gmw_init_msk_img'] = gmw_init_msk_img
                 c_dict['water_msk_img'] = water_msk_img
+                c_dict['water_chng_img'] = water_chng_img
                 c_dict['pot_chng_rgns_img'] = pot_chng_rgns_img
                 c_dict['out_img'] = out_img
                 self.params.append(c_dict)
@@ -36,6 +38,7 @@ class GenCmds(PBPTGenQProcessToolCmds):
         self.gen_command_info(gmw_tiles='/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_2010_v3/*.kea',
                               gmw_init_tiles='/scratch/a.pfb/gmw_v2_gapfill/data/gmw_tiles/gmw_init_v3_further_qa_part2',
                               water_msk_dir='/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_2010_init_water_mask',
+                              water_change_dir='/scratch/a.pfb/gmw_v3_change/data/other_base_data/water_change_gmw_tiles',
                               potent_chg_rgns_dir='/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/pot_gmw_chng_ocean_chg_rgns_nasa_gmwv2_updates_qad',
                               out_dir='/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_2010_fnl_potent_chg_rgn')
         self.pop_params_db()
