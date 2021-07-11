f7a4212559560ce237f6a840552ee41c44a6cd75
Pete Bunting <petebunting@mac.com>
2021-07-11 19:07:15 +0100

    Update to check raw change features.

--- 00_utils/run_chk_imgs.sh
+++ 00_utils/run_chk_imgs.sh
@@ -49,8 +49,8 @@ singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 -i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2020_chngs/*.kea"
 
 
-singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_imgs.py \
---nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_2010_fnl_potent_chg_rgn/*.kea"
+#singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+#/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_imgs.py \
+#--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
+#-i "/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_2010_fnl_potent_chg_rgn/*.kea"
 
