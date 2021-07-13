a91ba082cb4545beb887699ac482bd5d44b5bc9a
Pete Bunting <petebunting@mac.com>
2021-07-13 16:41:43 +0100

    Update

--- 00_utils/run_chk_h5s.sh
+++ 00_utils/run_chk_h5s.sh
@@ -1,46 +1,6 @@
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_hdf5_files.py --rmerr --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_core_1996_pxl_vals/*.h5"
-
-singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_hdf5_files.py --rmerr --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_core_2007_pxl_vals/*.h5"
-
-singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_hdf5_files.py --rmerr --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_core_2008_pxl_vals/*.h5"
-
-singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_hdf5_files.py --rmerr --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_core_2009_pxl_vals/*.h5"
-
-singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_hdf5_files.py --rmerr --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_core_2010_pxl_vals/*.h5"
-
-singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_hdf5_files.py --rmerr --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_core_2015_pxl_vals/*.h5"
-
-singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_hdf5_files.py --rmerr --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_core_2016_pxl_vals/*.h5"
-
-singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_hdf5_files.py --rmerr --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_core_2017_pxl_vals/*.h5"
-
-singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_hdf5_files.py --rmerr --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_core_2018_pxl_vals/*.h5"
-
-singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_hdf5_files.py --rmerr --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_core_2019_pxl_vals/*.h5"
-
-singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_hdf5_files.py --rmerr --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_core_2020_pxl_vals/*.h5"
+-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2010_pxl_vals/*.h5"
 
 
 
