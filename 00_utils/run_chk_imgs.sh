bbb9edf6af7ee2ca3cc6baa7cd82f7552d44b5ad
Pete Bunting <petebunting@mac.com>
2021-08-08 13:05:45 +0100

    Update check images

--- 00_utils/run_chk_imgs.sh
+++ 00_utils/run_chk_imgs.sh
@@ -1,60 +1,44 @@
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_imgs.py \
---nbands 3 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/jaxa_tiles/2020/*/*db_mskd_reg.kea"
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
+-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2007_chngs/*.kea"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_imgs.py \
---nbands 3 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/jaxa_tiles/2019/*/*db_mskd_reg.kea"
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
+-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2008_chngs/*.kea"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_imgs.py \
---nbands 3 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/jaxa_tiles/2018/*/*db_mskd_reg.kea"
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
+-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2009_chngs/*.kea"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_imgs.py \
---nbands 3 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/jaxa_tiles/2017/*/*db_mskd_reg.kea"
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
+-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2015_chngs/*.kea"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_imgs.py \
---nbands 3 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/jaxa_tiles/2016/*/*db_mskd_reg.kea"
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
+-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2016_chngs/*.kea"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_imgs.py \
---nbands 3 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/jaxa_tiles/2015/*/*db_mskd_reg.kea"
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
+-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2017_chngs/*.kea"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_imgs.py \
---nbands 3 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/jaxa_tiles/2009/*/*db_mskd_reg.kea"
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
+-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2018_chngs/*.kea"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_imgs.py \
---nbands 3 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/jaxa_tiles/2008/*/*db_mskd_reg.kea"
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
+-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2019_chngs/*.kea"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_imgs.py \
---nbands 3 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/jaxa_tiles/2007/*/*db_mskd_reg.kea"
-
-#singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-#/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_imgs.py \
-#--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
-#-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2020_chngs/*mng_chng.kea"
-
-
-#singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-#/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_imgs.py \
-#--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
-#-i "/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_2010_fnl_potent_chg_rgn/*.kea"
-
-#singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-#/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_imgs.py \
-#--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
-#-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2010_chngs/*mng_chng.kea"
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
+-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2020_chngs/*.kea"
