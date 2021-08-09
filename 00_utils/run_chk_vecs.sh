216c7a5fa12bc2de8c18b48daece6ca0123cfeac
Pete Bunting <petebunting@mac.com>
2021-08-09 11:11:50 +0100

    Update to just check 2007-2020 as these were updated.

--- 00_utils/run_chk_vecs.sh
+++ 00_utils/run_chk_vecs.sh
@@ -1,7 +1,7 @@
-singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_vec_files.py \
---multi --rmerr --chkproj --epsg 4326 --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_1996_chngs_vecs/*.gpkg"
+#singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+#/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_vec_files.py \
+#--multi --rmerr --chkproj --epsg 4326 --printerrs \
+#-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_1996_chngs_vecs/*.gpkg"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_vec_files.py \
