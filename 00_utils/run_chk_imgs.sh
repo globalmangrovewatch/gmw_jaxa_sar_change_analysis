d5d8960c795298a2e630d8b0b51b18eedfac6414
Pete Bunting <petebunting@mac.com>
2021-08-25 14:46:15 +0100

    Update to check output images.

--- 00_utils/run_chk_imgs.sh
+++ 00_utils/run_chk_imgs.sh
@@ -1,15 +1,7 @@
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_imgs.py \
 --nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/jaxa_tiles/dmaxmin_dB/*.kea"
+-i "/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_v3_init_change_regions/*.kea"
 
-singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_imgs.py \
---nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/jaxa_tiles/max_dB/*.kea"
 
-singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_imgs.py \
---nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/jaxa_tiles/min_dB/*.kea"
 
