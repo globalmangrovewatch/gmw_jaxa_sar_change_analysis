1b15418daffb417934471840662905672e7f40b1
Pete Bunting <petebunting@mac.com>
2021-09-02 08:58:19 +0100

    Update to check vector output files

--- 00_utils/run_chk_vecs.sh
+++ 00_utils/run_chk_vecs.sh
@@ -2,5 +2,5 @@
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_vec_files.py \
 --multi --rmerr --chkproj --epsg 4326 --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/pot_gmw_chng_ocean_chg_rgns_nasa_gmwv2_updates_vecs/*.gpkg"
+-i "/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_2010_fnl_potent_chg_rgn_vecs/*.gpkg"
 
