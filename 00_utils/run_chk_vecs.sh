ffb753641d8ce8ea96ac8fb0e903e8d5c669b560
Pete Bunting <petebunting@mac.com>
2021-09-09 16:34:24 +0100

    Update to check vector files.

--- 00_utils/run_chk_vecs.sh
+++ 00_utils/run_chk_vecs.sh
@@ -2,5 +2,5 @@
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python chk_vec_files.py \
 --multi --rmerr --chkproj --epsg 4326 --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_2010_fnl_potent_chg_rgn_vecs/*.gpkg"
+-i "/scratch/a.pfb/gmw_v3_change/data/gmw_baseline/gmw_2010_fnl_potent_chg_rgn_qad_rm_sml_vecs/*.gpkg"
 
