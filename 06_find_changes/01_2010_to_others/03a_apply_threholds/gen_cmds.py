e9c2366ef188500da28906cc55a061f6819925fe
Pete Bunting <petebunting@mac.com>
2021-09-14 10:37:14 +0100

    Update to get correct 1996 file name

--- 06_find_changes/01_2010_to_others/03a_apply_threholds/gen_cmds.py
+++ 06_find_changes/01_2010_to_others/03a_apply_threholds/gen_cmds.py
@@ -26,7 +26,10 @@ class GenCmds(PBPTGenQProcessToolCmds):
 
             sar_scn_dir = os.path.join(kwargs['sar_tiles_dir'], tile_name)
             if os.path.exists(sar_scn_dir):
-                sar_img = os.path.join(sar_scn_dir, '{}_{}_db_mskd_reg.kea'.format(tile_name, kwargs['sar_year']))
+                if kwargs['sar_year'] == '1996':
+                    sar_img = os.path.join(sar_scn_dir, '{}_1996_db.kea'.format(tile_name))
+                else:
+                    sar_img = os.path.join(sar_scn_dir, '{}_{}_db_mskd_reg.kea'.format(tile_name, kwargs['sar_year']))
                 if not os.path.exists(sar_img):
                     sar_img = None
             else:
