8a0f2b820573c1beeaab9da95d8400822ad6548f
Pete Bunting <petebunting@mac.com>
2021-09-15 21:42:44 +0100

    Add scripts to apply thresholds to SAR scenes.

---
+++ 06_find_changes/03_all_change_years/03a_apply_threholds/create_img_tile.py
@@ -0,0 +1,253 @@
+from pbprocesstools.pbpt_q_process import PBPTQProcessTool
+import logging
+import os
+import shutil
+import math
+import pprint
+import rsgislib
+import rsgislib.rastergis
+import rsgislib.imagecalc
+import rsgislib.imageutils
+
+def create_1996_mng_msk(gmw_tile, sar_img, sar_vld_img, threshold, threshold_se, out_img, out_lower_img, out_upper_img, calc_intervals=True):
+    # 95th intervals
+    threshold_upper = threshold + (threshold_se * 1.96)
+    threshold_lower = threshold - (threshold_se * 1.96)
+
+    # Using threshold
+    band_defns = []
+    band_defns.append(rsgislib.imagecalc.BandDefn('gmw', gmw_tile, 1))
+    band_defns.append(rsgislib.imagecalc.BandDefn('hh', sar_img, 1))
+    band_defns.append(rsgislib.imagecalc.BandDefn('vld', sar_vld_img, 1))
+    exp = '(vld==1)&&(gmw==1)&&(hh<{})?1:0'.format(threshold)
+    rsgislib.imagecalc.bandMath(out_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+    rsgislib.rastergis.populateStats(out_img, True, True, True)
+
+    if calc_intervals:
+        # Using upper threshold
+        band_defns = []
+        band_defns.append(rsgislib.imagecalc.BandDefn('gmw', gmw_tile, 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('hh', sar_img, 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('vld', sar_vld_img, 1))
+        exp = '(vld==1)&&(gmw==1)&&(hh<{})?1:0'.format(threshold_upper)
+        rsgislib.imagecalc.bandMath(out_upper_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+        rsgislib.rastergis.populateStats(out_upper_img, True, True, True)
+
+        # Using lower threshold
+        band_defns = []
+        band_defns.append(rsgislib.imagecalc.BandDefn('gmw', gmw_tile, 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('hh', sar_img, 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('vld', sar_vld_img, 1))
+        exp = '(vld==1)&&(gmw==1)&&(hh<{})?1:0'.format(threshold_lower)
+        rsgislib.imagecalc.bandMath(out_lower_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+        rsgislib.rastergis.populateStats(out_lower_img, True, True, True)
+
+
+def create_1996_nmng_msk(chng_rgn_tile, sar_img, sar_vld_img, threshold, threshold_se, out_img, out_lower_img, out_upper_img, calc_intervals=True):
+    # 95th intervals
+    threshold_upper = threshold + (threshold_se * 1.96)
+    threshold_lower = threshold - (threshold_se * 1.96)
+
+    # Using threshold
+    band_defns = []
+    band_defns.append(rsgislib.imagecalc.BandDefn('chgmsk', chng_rgn_tile, 1))
+    band_defns.append(rsgislib.imagecalc.BandDefn('hh', sar_img, 1))
+    band_defns.append(rsgislib.imagecalc.BandDefn('vld', sar_vld_img, 1))
+    exp = '(vld==1)&&(chgmsk==1)&&(hh>{})?1:0'.format(threshold)
+    rsgislib.imagecalc.bandMath(out_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+    rsgislib.rastergis.populateStats(out_img, True, True, True)
+
+    if calc_intervals:
+        # Using upper threshold
+        band_defns = []
+        band_defns.append(rsgislib.imagecalc.BandDefn('chgmsk', chng_rgn_tile, 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('hh', sar_img, 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('vld', sar_vld_img, 1))
+        exp = '(vld==1)&&(chgmsk==1)&&(hh>{})?1:0'.format(threshold_upper)
+        rsgislib.imagecalc.bandMath(out_upper_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+        rsgislib.rastergis.populateStats(out_upper_img, True, True, True)
+
+        # Using lower threshold
+        band_defns = []
+        band_defns.append(rsgislib.imagecalc.BandDefn('chgmsk', chng_rgn_tile, 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('hh', sar_img, 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('vld', sar_vld_img, 1))
+        exp = '(vld==1)&&(chgmsk==1)&&(hh>{})?1:0'.format(threshold_lower)
+        rsgislib.imagecalc.bandMath(out_lower_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+        rsgislib.rastergis.populateStats(out_lower_img, True, True, True)
+
+
+
+
+def create_alos_mng_msk(gmw_tile, sar_img, sar_vld_img, threshold, threshold_se, out_img, out_lower_img, out_upper_img, calc_intervals=True):
+    # 95th intervals
+    threshold_upper = threshold + (threshold_se * 1.96)
+    threshold_lower = threshold - (threshold_se * 1.96)
+
+    # Using threshold
+    band_defns = []
+    band_defns.append(rsgislib.imagecalc.BandDefn('gmw', gmw_tile, 1))
+    band_defns.append(rsgislib.imagecalc.BandDefn('hv', sar_img, 2))
+    band_defns.append(rsgislib.imagecalc.BandDefn('vld', sar_vld_img, 1))
+    exp = '(vld==1)&&(gmw==1)&&(hv<{})?1:0'.format(threshold)
+    rsgislib.imagecalc.bandMath(out_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+    rsgislib.rastergis.populateStats(out_img, True, True, True)
+
+    if calc_intervals:
+        # Using upper threshold
+        band_defns = []
+        band_defns.append(rsgislib.imagecalc.BandDefn('gmw', gmw_tile, 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('hv', sar_img, 2))
+        band_defns.append(rsgislib.imagecalc.BandDefn('vld', sar_vld_img, 1))
+        exp = '(vld==1)&&(gmw==1)&&(hv<{})?1:0'.format(threshold_upper)
+        rsgislib.imagecalc.bandMath(out_upper_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+        rsgislib.rastergis.populateStats(out_upper_img, True, True, True)
+
+        # Using lower threshold
+        band_defns = []
+        band_defns.append(rsgislib.imagecalc.BandDefn('gmw', gmw_tile, 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('hv', sar_img, 2))
+        band_defns.append(rsgislib.imagecalc.BandDefn('vld', sar_vld_img, 1))
+        exp = '(vld==1)&&(gmw==1)&&(hv<{})?1:0'.format(threshold_lower)
+        rsgislib.imagecalc.bandMath(out_lower_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+        rsgislib.rastergis.populateStats(out_lower_img, True, True, True)
+
+
+
+def create_alos_nmng_msk(chng_rgn_tile, sar_img, sar_vld_img, threshold, threshold_se, out_img, out_lower_img, out_upper_img, calc_intervals=True):
+    # 95th intervals
+    threshold_upper = threshold + (threshold_se * 1.96)
+    threshold_lower = threshold - (threshold_se * 1.96)
+
+    # Using threshold
+    band_defns = []
+    band_defns.append(rsgislib.imagecalc.BandDefn('chgmsk', chng_rgn_tile, 1))
+    band_defns.append(rsgislib.imagecalc.BandDefn('hv', sar_img, 2))
+    band_defns.append(rsgislib.imagecalc.BandDefn('vld', sar_vld_img, 1))
+    exp = '(vld==1)&&(chgmsk==1)&&(hv>{})?1:0'.format(threshold)
+    rsgislib.imagecalc.bandMath(out_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+    rsgislib.rastergis.populateStats(out_img, True, True, True)
+
+    if calc_intervals:
+        # Using upper threshold
+        band_defns = []
+        band_defns.append(rsgislib.imagecalc.BandDefn('chgmsk', chng_rgn_tile, 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('hv', sar_img, 2))
+        band_defns.append(rsgislib.imagecalc.BandDefn('vld', sar_vld_img, 1))
+        exp = '(vld==1)&&(chgmsk==1)&&(hv>{})?1:0'.format(threshold_upper)
+        rsgislib.imagecalc.bandMath(out_upper_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+        rsgislib.rastergis.populateStats(out_upper_img, True, True, True)
+
+        # Using lower threshold
+        band_defns = []
+        band_defns.append(rsgislib.imagecalc.BandDefn('chgmsk', chng_rgn_tile, 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('hv', sar_img, 2))
+        band_defns.append(rsgislib.imagecalc.BandDefn('vld', sar_vld_img, 1))
+        exp = '(vld==1)&&(chgmsk==1)&&(hv>{})?1:0'.format(threshold_lower)
+        rsgislib.imagecalc.bandMath(out_lower_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defns)
+        rsgislib.rastergis.populateStats(out_lower_img, True, True, True)
+
+logger = logging.getLogger(__name__)
+
+class CreateImageTile(PBPTQProcessTool):
+
+    def __init__(self):
+        super().__init__(cmd_name='create_img_tile.py', descript=None)
+
+    def do_processing(self, **kwargs):
+        if not os.path.exists(self.params['tmp_dir']):
+            os.mkdir(self.params['tmp_dir'])
+
+        if self.params['sar_img'] is not None:
+            rsgis_utils = rsgislib.RSGISPyUtils()
+            thres_lut = rsgis_utils.readJSON2Dict(self.params['gmw_proj_thres_file'])
+            pprint.pprint(thres_lut)
+
+            sar_vld_img = os.path.join(self.params['tmp_dir'], "{}_sar_vld_msk.kea".format(self.params['tile']))
+            rsgislib.imageutils.genValidMask(self.params['sar_img'], sar_vld_img, "KEA", nodata=32767)
+
+            if self.params['sar_year'] == '1996':
+                mng_hh_thres = thres_lut[self.params['sar_year']]['mng_hh']
+                mng_hh_thres_se = thres_lut[self.params['sar_year']]['mng_hh_se']
+                if math.isnan(mng_hh_thres_se):
+                    mng_hh_thres_se = 0.0
+                create_1996_mng_msk(self.params['gmw_tile'], self.params['sar_img'], sar_vld_img, mng_hh_thres, mng_hh_thres_se, self.params['out_mng_chng'], self.params['out_mng_chng_lower'], self.params['out_mng_chng_upper'], self.params['calc_intervals'])
+
+                nmng_hh_thres = thres_lut[self.params['sar_year']]['nmng_hh']
+                nmng_hh_thres_se = thres_lut[self.params['sar_year']]['nmng_hh_se']
+                if math.isnan(nmng_hh_thres_se):
+                    nmng_hh_thres_se = 0.0
+                create_1996_nmng_msk(self.params['potent_chng_msk_img'], self.params['sar_img'], sar_vld_img, nmng_hh_thres, nmng_hh_thres_se, self.params['out_nmng_chng'], self.params['out_nmng_chng_lower'], self.params['out_nmng_chng_upper'], self.params['calc_intervals'])
+
+            else:
+                mng_hv_thres = thres_lut[self.params['sar_year']]['mng_hv']
+                mng_hv_thres_se = thres_lut[self.params['sar_year']]['mng_hv_se']
+                if math.isnan(mng_hv_thres_se):
+                    mng_hv_thres_se = 0.0
+                create_alos_mng_msk(self.params['gmw_tile'], self.params['sar_img'], sar_vld_img, mng_hv_thres, mng_hv_thres_se, self.params['out_mng_chng'], self.params['out_mng_chng_lower'], self.params['out_mng_chng_upper'], self.params['calc_intervals'])
+
+                nmng_hv_thres = thres_lut[self.params['sar_year']]['nmng_hv']
+                nmng_hv_thres_se = thres_lut[self.params['sar_year']]['nmng_hv_se']
+                if math.isnan(nmng_hv_thres_se):
+                    nmng_hv_thres_se = 0.0
+                create_alos_nmng_msk(self.params['potent_chng_msk_img'], self.params['sar_img'], sar_vld_img, nmng_hv_thres, nmng_hv_thres_se, self.params['out_nmng_chng'], self.params['out_nmng_chng_lower'], self.params['out_nmng_chng_upper'], self.params['calc_intervals'])
+        else:
+            rsgislib.imagecalc.imageMath(self.params['gmw_tile'], self.params['out_mng_chng'], '0', 'KEA', rsgislib.TYPE_8UINT)
+            rsgislib.imagecalc.imageMath(self.params['gmw_tile'], self.params['out_nmng_chng'], '0', 'KEA', rsgislib.TYPE_8UINT)
+            if self.params['calc_intervals']:
+                rsgislib.imagecalc.imageMath(self.params['gmw_tile'], self.params['out_mng_chng_lower'], '0', 'KEA', rsgislib.TYPE_8UINT)
+                rsgislib.imagecalc.imageMath(self.params['gmw_tile'], self.params['out_nmng_chng_lower'], '0', 'KEA', rsgislib.TYPE_8UINT)
+                rsgislib.imagecalc.imageMath(self.params['gmw_tile'], self.params['out_mng_chng_upper'], '0', 'KEA', rsgislib.TYPE_8UINT)
+                rsgislib.imagecalc.imageMath(self.params['gmw_tile'], self.params['out_nmng_chng_upper'], '0', 'KEA', rsgislib.TYPE_8UINT)
+
+        if os.path.exists(self.params['tmp_dir']):
+            shutil.rmtree(self.params['tmp_dir'])
+
+
+    def required_fields(self, **kwargs):
+        return ["tile", "gmw_tile", "sar_year", "potent_chng_msk_img", "sar_img",
+                "gmw_proj_thres_file", "out_mng_chng", "out_nmng_chng", "out_mng_chng_upper",
+                "out_nmng_chng_upper", "out_mng_chng_lower", "out_nmng_chng_lower",
+                "calc_intervals", "tmp_dir"]
+
+    def outputs_present(self, **kwargs):
+        files_dict = dict()
+        files_dict[self.params['out_mng_chng']] = 'gdal_image'
+        files_dict[self.params['out_nmng_chng']] = 'gdal_image'
+        if self.params['calc_intervals']:
+            files_dict[self.params['out_mng_chng_upper']] = 'gdal_image'
+            files_dict[self.params['out_nmng_chng_upper']] = 'gdal_image'
+            files_dict[self.params['out_mng_chng_lower']] = 'gdal_image'
+            files_dict[self.params['out_nmng_chng_lower']] = 'gdal_image'
+        return self.check_files(files_dict)
+
+    def remove_outputs(self, **kwargs):
+        # Remove the output files.
+        if os.path.exists(self.params['out_mng_chng']):
+            os.remove(self.params['out_mng_chng'])
+
+        if os.path.exists(self.params['out_nmng_chng']):
+            os.remove(self.params['out_nmng_chng'])
+
+        if self.params['calc_intervals']:
+            if os.path.exists(self.params['out_mng_chng_upper']):
+                os.remove(self.params['out_mng_chng_upper'])
+
+            if os.path.exists(self.params['out_nmng_chng_upper']):
+                os.remove(self.params['out_nmng_chng_upper'])
+
+            if os.path.exists(self.params['out_mng_chng_lower']):
+                os.remove(self.params['out_mng_chng_lower'])
+
+            if os.path.exists(self.params['out_nmng_chng_lower']):
+                os.remove(self.params['out_nmng_chng_lower'])
+
+        # Reset the tmp dir
+        if os.path.exists(self.params['tmp_dir']):
+            shutil.rmtree(self.params['tmp_dir'])
+        os.mkdir(self.params['tmp_dir'])
+
+if __name__ == "__main__":
+    CreateImageTile().std_run()
+
+
---
+++ 06_find_changes/03_all_change_years/03a_apply_threholds/gen_cmds.py
@@ -0,0 +1,120 @@
+from pbprocesstools.pbpt_q_process import PBPTGenQProcessToolCmds
+import logging
+import os
+import glob
+import rsgislib
+
+logger = logging.getLogger(__name__)
+
+class GenCmds(PBPTGenQProcessToolCmds):
+
+    def gen_command_info(self, **kwargs):
+        if not os.path.exists(kwargs['out_dir']):
+            os.mkdir(kwargs['out_dir'])
+
+        rsgis_utils = rsgislib.RSGISPyUtils()
+        tile_prj_lut = rsgis_utils.readJSON2Dict(kwargs['prjs_lut_file'])
+
+        img_tiles = glob.glob(kwargs['gmw_tiles'])
+        for gmw_tile in img_tiles:
+            basename = self.get_file_basename(gmw_tile)
+            tile_basename = self.get_file_basename(gmw_tile, n_comps=2)
+            tile_name = tile_basename.split('_')[1]
+
+            gmw_prj = tile_prj_lut[tile_name]
+            gmw_proj_thres_file = os.path.join(kwargs['thres_files_dir'], "{}_fnl_thresholds.json".format(gmw_prj))
+
+            sar_scn_dir = os.path.join(kwargs['sar_tiles_dir'], tile_name)
+            if os.path.exists(sar_scn_dir):
+                if kwargs['sar_year'] == '1996':
+                    sar_img = os.path.join(sar_scn_dir, '{}_1996_db.kea'.format(tile_name))
+                else:
+                    sar_img = os.path.join(sar_scn_dir, '{}_{}_db_mskd_reg.kea'.format(tile_name, kwargs['sar_year']))
+                if not os.path.exists(sar_img):
+                    sar_img = None
+            else:
+                sar_img = None
+
+            potent_chng_msk_img = os.path.join(kwargs['potent_chng_msk_dir'], '{}_{}_pchg_v3_base2010.kea'.format(tile_basename, kwargs['base_year']))
+
+            out_mng_chng = os.path.join(kwargs['out_dir'], '{}_{}_mng_chng_base{}.kea'.format(tile_basename, kwargs['sar_year'], kwargs['base_year']))
+            out_nmng_chng = os.path.join(kwargs['out_dir'], '{}_{}_not_mng_chng_base{}.kea'.format(tile_basename, kwargs['sar_year'], kwargs['base_year']))
+
+            out_mng_chng_upper = os.path.join(kwargs['out_dir'], '{}_{}_mng_chng_upper_base{}.kea'.format(tile_basename, kwargs['sar_year'], kwargs['base_year']))
+            out_nmng_chng_upper = os.path.join(kwargs['out_dir'], '{}_{}_not_mng_chng_upper_base{}.kea'.format(tile_basename, kwargs['sar_year'], kwargs['base_year']))
+
+            out_mng_chng_lower = os.path.join(kwargs['out_dir'], '{}_{}_mng_chng_lower_base{}.kea'.format(tile_basename, kwargs['sar_year'], kwargs['base_year']))
+            out_nmng_chng_lower = os.path.join(kwargs['out_dir'], '{}_{}_not_mng_chng_lower_base{}.kea'.format(tile_basename, kwargs['sar_year'], kwargs['base_year']))
+
+            calc_intervals = False
+
+            out_files_present = True
+            if (not os.path.exists(out_mng_chng)) or (not os.path.exists(out_nmng_chng)):
+                out_files_present = False
+            elif calc_intervals:
+                if (os.path.exists(out_mng_chng_upper)) or (not os.path.exists(out_nmng_chng_upper)) or (not os.path.exists(out_mng_chng_lower)) or (not os.path.exists(out_nmng_chng_lower)):
+                    out_files_present = False
+
+            if  not out_files_present:
+                #if (sar_vld_img is not None) and os.path.exists(sar_vld_img):
+                #    print("rm {}".format(sar_vld_img))
+                c_dict = dict()
+                c_dict['tile'] = tile_basename
+                c_dict['gmw_tile'] = gmw_tile
+                c_dict['sar_year'] = kwargs['sar_year']
+                c_dict['potent_chng_msk_img'] = potent_chng_msk_img
+                c_dict['sar_img'] = sar_img
+                c_dict['gmw_proj_thres_file'] = gmw_proj_thres_file
+                c_dict['out_mng_chng'] = out_mng_chng
+                c_dict['out_nmng_chng'] = out_nmng_chng
+                c_dict['out_mng_chng_upper'] = out_mng_chng_upper
+                c_dict['out_nmng_chng_upper'] = out_nmng_chng_upper
+                c_dict['out_mng_chng_lower'] = out_mng_chng_lower
+                c_dict['out_nmng_chng_lower'] = out_nmng_chng_lower
+                c_dict['calc_intervals'] = calc_intervals
+                c_dict['tmp_dir'] = os.path.join(kwargs['tmp_dir'], "{}_{}_base{}_apply_chng_thres".format(tile_basename, kwargs['sar_year'], kwargs['base_year']))
+                if not os.path.exists(c_dict['tmp_dir']):
+                    os.mkdir(c_dict['tmp_dir'])
+                self.params.append(c_dict)
+
+
+    def run_gen_commands(self):
+        all_years = ['1996', '2007', '2008', '2009', '2010', '2015', '2016', '2017', '2018', '2019', '2020']
+        years_l1 = ['1996']#, '2007', '2008', '2009', '2015', '2016', '2017', '2018', '2019', '2020']
+        for l1_year in years_l1:
+            base_dir = '/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/from{}'.format(l1_year)
+            chng_years = all_years.copy()
+            chng_years.remove(l1_year)
+            for chg_year in chng_years:
+                sar_tiles_dir = '/scratch/a.pfb/gmw_v3_change/data/jaxa_tiles/{}'.format(chg_year)
+                if chg_year == '1996':
+                    sar_tiles_dir = '/scratch/a.pfb/gmw_v3_change/data/jaxa_tiles/1996_v2_reg'
+
+                self.gen_command_info(gmw_tiles='/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/from2010/gmw_base2010_mng_{}_v3/*.kea'.format(l1_year),
+                                      prjs_lut_file='/scratch/a.pfb/gmw_v3_change/scripts/03_prepare_datasets/09_create_project_tile_lut/gmw_tiles_luts.json',
+                                      potent_chng_msk_dir='/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/from2010/gmw_base2010_pchg_{}_v3'.format(l1_year),
+                                      sar_tiles_dir=sar_tiles_dir,
+                                      sar_year=chg_year,
+                                      base_year=l1_year,
+                                      thres_files_dir=os.path.join(base_dir, 'fnl_prj_thresholds'),
+                                      out_dir=os.path.join(base_dir, 'gmw_{}_{}_chngs'.format(l1_year, chg_year)),
+                                      tmp_dir='/scratch/a.pfb/gmw_v3_change/tmp')
+
+        self.pop_params_db()
+        self.create_slurm_sub_sh("gmw_XXXX_XXXX_chngs", 16448, '/scratch/a.pfb/gmw_v3_change/logs',
+                                 run_script='run_exe_analysis.sh', job_dir="job_scripts",
+                                 db_info_file=None, account_name='scw1376', n_cores_per_job=10, n_jobs=10,
+                                 job_time_limit='2-23:59',
+                                 module_load='module load parallel singularity\n\nexport http_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\nexport https_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\n')
+
+if __name__ == "__main__":
+    py_script = os.path.abspath("create_img_tile.py")
+    script_cmd = "singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python {}".format(py_script)
+
+    process_tools_mod = 'create_img_tile'
+    process_tools_cls = 'CreateImageTile'
+
+    create_tools = GenCmds(cmd=script_cmd, db_conn_file="/home/a.pfb/gmw_gap_fill_db/pbpt_db_conn.txt",
+                                         lock_file_path="./gmw_gapfill_lock_file.txt",
+                                         process_tools_mod=process_tools_mod, process_tools_cls=process_tools_cls)
+    create_tools.parse_cmds()
