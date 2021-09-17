90b60709c407c49e2c50af1d3ac158d0dc42fbbf
Pete Bunting <petebunting@mac.com>
2021-09-17 10:56:13 +0100

    Update scripts to summarise PALSAR 2010 baseline update.

--- 05_define_2010_baseline/05_run_change_analysis/04c_summarise_raw_change_stats/summarise_raw_chng_stats.py
+++ 05_define_2010_baseline/05_run_change_analysis/04c_summarise_raw_change_stats/summarise_raw_chng_stats.py
@@ -32,9 +32,7 @@ tile_lut_dict.pop("N13E045")
 tile_idx_lst = tile_lut_dict.keys()
 
 
-in_data_dir = "/Users/pete/Temp/gmw_v3_analysis/v3_2010_refine_chng_thresholds/outputs/gmw_2010_2010_chngs_stats"
-
-#in_data_dir = "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data"
+in_data_dir = "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/from2010/gmw_2010_2010_chngs_stats"
 
 mng_ext_2010 = get_data(tile_idx_lst, in_data_dir, '2010')
 mng_to_nmng = get_data(tile_idx_lst, in_data_dir, 'chng_mng')
@@ -47,9 +45,8 @@ out_data['nmng to mng'] = nmng_to_mng
 
 df_data = pandas.DataFrame(out_data, index=tile_idx_lst)
 
-
 # Create a Pandas Excel writer using XlsxWriter as the engine.
-xls_writer = pandas.ExcelWriter('gmw_tile_2010_raw_chng_feat_stats.xlsx', engine='xlsxwriter')
+xls_writer = pandas.ExcelWriter('gmw_tile_2010base_2010palsar_change_304.xlsx', engine='xlsxwriter')
 
 df_data.to_excel(xls_writer, sheet_name='2010_chng')
 
