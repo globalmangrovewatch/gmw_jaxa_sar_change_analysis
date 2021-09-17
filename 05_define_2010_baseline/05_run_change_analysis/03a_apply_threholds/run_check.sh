69fd1ca3cf23e62ccd31e2a5b486d1a4a08e7efd
Pete Bunting <petebunting@mac.com>
2021-09-17 10:35:50 +0100

    Update file path

--- 05_define_2010_baseline/05_run_change_analysis/03a_apply_threholds/run_check.sh
+++ 05_define_2010_baseline/05_run_change_analysis/03a_apply_threholds/run_check.sh
@@ -3,6 +3,6 @@
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../../00_utils/chk_imgs.py \
 --nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 1  --printerrs \
--i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_2010_2010_chngs/*.kea"
+-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/from2010/gmw_2010_2010_chngs/*.kea"
 
 
