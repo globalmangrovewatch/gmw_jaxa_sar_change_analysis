cdab0cfb8489601e3ddd33e173ff96dac24d0ed2
Pete Bunting <petebunting@mac.com>
2021-09-19 14:37:06 +0100

    Add script to run all checks.

---
+++ 06_find_changes/03_all_change_years/03a_apply_threholds/run_check_all.sh
@@ -0,0 +1,11 @@
+#sh run_check_1996.sh
+sh run_check_2007.sh
+sh run_check_2008.sh
+sh run_check_2009.sh
+sh run_check_2015.sh
+sh run_check_2016.sh
+sh run_check_2017.sh
+sh run_check_2018.sh
+sh run_check_2019.sh
+sh run_check_2020.sh
+
