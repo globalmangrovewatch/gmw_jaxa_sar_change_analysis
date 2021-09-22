ae63e1449bcbb89c4f0fc4849454d701a895f3fb
Pete Bunting <petebunting@mac.com>
2021-09-22 14:50:10 +0100

    another bug fix.

--- 06_find_changes/04_merge_all_change_years/01_create_summarised_layers/gen_cmds.py
+++ 06_find_changes/04_merge_all_change_years/01_create_summarised_layers/gen_cmds.py
@@ -24,7 +24,7 @@ class GenCmds(PBPTGenQProcessToolCmds):
                     mng_ext_img = gmw_tile
                 mng_ext_imgs.append(mng_ext_img)
 
-            out_gmw_mng_sum_img = os.path.join(kwargs['out_dir'], '{}_{}_mng_v3_base{}.tif'.format(tile_basename, kwargs['sar_year'], kwargs['base_year']))
+            out_gmw_mng_sum_img = os.path.join(kwargs['out_dir'], '{}_{}_mng_sum_v3.tif'.format(tile_basename, kwargs['year']))
 
             if not os.path.exists(out_gmw_mng_sum_img):
                 c_dict = dict()
