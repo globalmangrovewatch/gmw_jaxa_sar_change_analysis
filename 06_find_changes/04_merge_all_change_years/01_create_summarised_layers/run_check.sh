929565cf18fa07f12abae4b35a6d93bb948cc58c
Pete Bunting <petebunting@mac.com>
2021-09-26 15:56:03 +0100

    Add option so check sum is calculated.

--- 06_find_changes/04_merge_all_change_years/01_create_summarised_layers/run_check.sh
+++ 06_find_changes/04_merge_all_change_years/01_create_summarised_layers/run_check.sh
@@ -2,57 +2,57 @@
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../../00_utils/chk_imgs.py \
---nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs \
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
 -i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_mng_ext_sum_1996/*.kea"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../../00_utils/chk_imgs.py \
---nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs \
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
 -i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_mng_ext_sum_2007/*.kea"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../../00_utils/chk_imgs.py \
---nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs \
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
 -i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_mng_ext_sum_2008/*.kea"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../../00_utils/chk_imgs.py \
---nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs \
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
 -i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_mng_ext_sum_2009/*.kea"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../../00_utils/chk_imgs.py \
---nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs \
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
 -i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_mng_ext_sum_2010/*.kea"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../../00_utils/chk_imgs.py \
---nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs \
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
 -i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_mng_ext_sum_2015/*.kea"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../../00_utils/chk_imgs.py \
---nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs \
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
 -i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_mng_ext_sum_2016/*.kea"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../../00_utils/chk_imgs.py \
---nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs \
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
 -i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_mng_ext_sum_2017/*.kea"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../../00_utils/chk_imgs.py \
---nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs \
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
 -i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_mng_ext_sum_2018/*.kea"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../../00_utils/chk_imgs.py \
---nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs \
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
 -i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_mng_ext_sum_2019/*.kea"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../../00_utils/chk_imgs.py \
---nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs \
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
 -i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_mng_ext_sum_2020/*.kea"
 
 
