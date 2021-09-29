9912e1b1b10cce73b5dde157a77b02e85a64215e
Pete Bunting <petebunting@mac.com>
2021-09-29 22:42:20 +0100

    Update removing comments.

--- 06_find_changes/04_merge_all_change_years/04_calc_tile_stats/run_calc_stats_mjr.sh
+++ 06_find_changes/04_merge_all_change_years/04_calc_tile_stats/run_calc_stats_mjr.sh
@@ -1,7 +1,7 @@
-#singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-#/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python calc_pxl_stats.py \
-#-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_v3_mng_mjr_ext_1996/*.kea" \
-#-o gmw_v3_mng_mjr_ext_1996.json
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python calc_pxl_stats.py \
+-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_v3_mng_mjr_ext_1996/*.kea" \
+-o gmw_v3_mng_mjr_ext_1996.json
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python calc_pxl_stats.py \
