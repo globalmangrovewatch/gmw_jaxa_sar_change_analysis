bfd8a0e4e24f9fa971891bf5799b37056f02640b
Pete Bunting <petebunting@mac.com>
2021-10-01 16:57:26 +0100

    Add scripts for creating the final min and max layers.

---
+++ 08_apply_fnl_qa/02_updated_min_max_extents/create_img_tile.py
@@ -0,0 +1,55 @@
+from pbprocesstools.pbpt_q_process import PBPTQProcessTool
+import logging
+import os
+import shutil
+import rsgislib
+import rsgislib.rastergis
+import rsgislib.imagecalc
+import rsgislib.vectorutils
+
+logger = logging.getLogger(__name__)
+
+class CreateImageTile(PBPTQProcessTool):
+
+    def __init__(self):
+        super().__init__(cmd_name='create_img_tile.py', descript=None)
+
+    def do_processing(self, **kwargs):
+        if not os.path.exists(self.params['tmp_dir']):
+            os.mkdir(self.params['tmp_dir'])
+
+        band_defns = [rsgislib.imagecalc.BandDefn('gmw', self.params['gmw_tile'], 1),
+                      rsgislib.imagecalc.BandDefn('min', self.params['min_ext_img'], 1)]
+        rsgislib.imagecalc.bandMath(self.params['out_min_ext_img'], '(gmw==0)&&(min==1)?0:min', 'KEA', rsgislib.TYPE_8UINT, band_defns)
+        rsgislib.rastergis.populateStats(self.params['out_min_ext_img'], addclrtab=True, calcpyramids=True, ignorezero=True)
+
+        band_defns = [rsgislib.imagecalc.BandDefn('gmw', self.params['gmw_tile'], 1),
+                      rsgislib.imagecalc.BandDefn('max', self.params['max_ext_img'], 1)]
+        rsgislib.imagecalc.bandMath(self.params['out_max_ext_img'], '(gmw==1)&&(max==0)?1:max', 'KEA', rsgislib.TYPE_8UINT, band_defns)
+        rsgislib.rastergis.populateStats(self.params['out_max_ext_img'], addclrtab=True, calcpyramids=True, ignorezero=True)
+
+        if os.path.exists(self.params['tmp_dir']):
+            shutil.rmtree(self.params['tmp_dir'])
+
+
+    def required_fields(self, **kwargs):
+        return ["tile", "gmw_tile", "min_ext_img", "out_min_ext_img", "max_ext_img", "out_max_ext_img"]
+
+
+    def outputs_present(self, **kwargs):
+        files_dict = dict()
+        files_dict[self.params['out_img']] = 'gdal_image'
+        return self.check_files(files_dict)
+
+    def remove_outputs(self, **kwargs):
+        # Remove the output files.
+        if os.path.exists(self.params['out_img']):
+            os.remove(self.params['out_img'])
+
+        if os.path.exists(self.params['tmp_dir']):
+            shutil.rmtree(self.params['tmp_dir'])
+
+if __name__ == "__main__":
+    CreateImageTile().std_run()
+
+
---
+++ 08_apply_fnl_qa/02_updated_min_max_extents/run_check_max.sh
@@ -0,0 +1,57 @@
+#singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python gen_cmds.py --check
+
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/chk_imgs.py \
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
+-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_v3_fnl_max_1996_v309/*.kea"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/chk_imgs.py \
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
+-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_v3_fnl_max_2007_v309/*.kea"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/chk_imgs.py \
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
+-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_v3_fnl_max_2008_v309/*.kea"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/chk_imgs.py \
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
+-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_v3_fnl_max_2009_v309/*.kea"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/chk_imgs.py \
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
+-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_v3_fnl_max_2010_v309/*.kea"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/chk_imgs.py \
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
+-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_v3_fnl_max_2015_v309/*.kea"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/chk_imgs.py \
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
+-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_v3_fnl_max_2016_v309/*.kea"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/chk_imgs.py \
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
+-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_v3_fnl_max_2017_v309/*.kea"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/chk_imgs.py \
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
+-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_v3_fnl_max_2018_v309/*.kea"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/chk_imgs.py \
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
+-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_v3_fnl_max_2019_v309/*.kea"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/chk_imgs.py \
+--nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
+-i "/scratch/a.pfb/gmw_v3_change/data/gmw_chng_data/gmw_v3_fnl_max_2020_v309/*.kea"
