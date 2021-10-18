5ee643b490b03ec799e149b922b6fdb1143fca15
Pete Bunting <petebunting@mac.com>
2021-10-18 19:07:36 +0100

    Update path

--- 08_apply_fnl_qa/06_create_east_india_chng_maps/01_rasterise_base_map/run_check.sh
+++ 08_apply_fnl_qa/06_create_east_india_chng_maps/01_rasterise_base_map/run_check.sh
@@ -2,6 +2,6 @@
 
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/chk_imgs.py \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../../00_utils/chk_imgs.py \
 --nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
 -i "/scratch/a.pfb/gmw_v3_change/data/gmw_east_india_edits/gmw_v3_east_india_base/*.kea"
