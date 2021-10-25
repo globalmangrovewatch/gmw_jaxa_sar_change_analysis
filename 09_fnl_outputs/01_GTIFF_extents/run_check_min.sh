f4a4648d38f8f780415c5df7ed67e6d269631c69
Pete Bunting <petebunting@mac.com>
2021-10-25 21:56:41 +0100

    Update to check outputs.

--- 09_fnl_outputs/01_GTIFF_extents/run_check_min.sh
+++ 09_fnl_outputs/01_GTIFF_extents/run_check_min.sh
@@ -4,54 +4,54 @@
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/chk_imgs.py \
 --nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
--i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_1996_v309/*.tif"
+-i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_1996_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/chk_imgs.py \
 --nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
--i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2007_v309/*.tif"
+-i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2007_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/chk_imgs.py \
 --nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
--i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2008_v309/*.tif"
+-i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2008_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/chk_imgs.py \
 --nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
--i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2009_v309/*.tif"
+-i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2009_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/chk_imgs.py \
 --nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
--i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2010_v309/*.tif"
+-i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2010_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/chk_imgs.py \
 --nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
--i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2015_v309/*.tif"
+-i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2015_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/chk_imgs.py \
 --nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
--i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2016_v309/*.tif"
+-i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2016_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/chk_imgs.py \
 --nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
--i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2017_v309/*.tif"
+-i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2017_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/chk_imgs.py \
 --nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
--i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2018_v309/*.tif"
+-i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2018_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/chk_imgs.py \
 --nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
--i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2019_v309/*.tif"
+-i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2019_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/chk_imgs.py \
 --nbands 1 --rmerr --chkproj --epsg 4326 --readimg --npxls 10  --printerrs --chksum \
--i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2020_v309/*.tif"
+-i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2020_v312/*.tif"
