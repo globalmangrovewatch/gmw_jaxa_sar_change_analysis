ef9861678bcf67224797f8171b75a780e2e535e0
Pete Bunting <petebunting@mac.com>
2021-10-27 17:07:47 +0100

    Update merge_vecs.py

--- 09_fnl_outputs/04_calc_annual_chng/02_create_vector_changes/merge_vecs.py
+++ 09_fnl_outputs/04_calc_annual_chng/02_create_vector_changes/merge_vecs.py
@@ -7,28 +7,28 @@ input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/gmw_chngs
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "f1996_t2007", False)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/gmw_chngs/gmw_v3_f2007_t2008_v312_vecs/*.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "f1996_t2008", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "f1996_t2008", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/gmw_chngs/gmw_v3_f2008_t2009_v312_vecs/*.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "f1996_t2009", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "f1996_t2009", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/gmw_chngs/gmw_v3_f2009_t2010_v312_vecs/*.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "f1996_t2010", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "f1996_t2010", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/gmw_chngs/gmw_v3_f2010_t2015_v312_vecs/*.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "f1996_t2015", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "f1996_t2015", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/gmw_chngs/gmw_v3_f2015_t2016_v312_vecs/*.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "f1996_t2016", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "f1996_t2016", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/gmw_chngs/gmw_v3_f2016_t2017_v312_vecs/*.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "f1996_t2017", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "f1996_t2017", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/gmw_chngs/gmw_v3_f2017_t2018_v312_vecs/*.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "f1996_t2018", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "f1996_t2018", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/gmw_chngs/gmw_v3_f2018_t2019_v312_vecs/*.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "f1996_t2019", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "f1996_t2019", True)
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/gmw_chngs/gmw_v3_f2019_t2020_v312_vecs/*.gpkg")
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "f1996_t2020", False)
+rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "f1996_t2020", True)
