29ccaf105b86198b53a8c302a1299af637e72a3d
Pete Bunting <petebunting@mac.com>
2021-10-29 12:58:59 +0100

    Update to remove files and also process min and max extents.

--- 09_fnl_outputs/01_GTIFF_extents/run_chk_file_size.sh
+++ 09_fnl_outputs/01_GTIFF_extents/run_chk_file_size.sh
@@ -1,43 +1,139 @@
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
--s 1000000 -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_1996_v312/*.tif"
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_1996_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
--s 1000000 -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2007_v312/*.tif"
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2007_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
--s 1000000 -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2008_v312/*.tif"
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2008_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
--s 1000000 -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2009_v312/*.tif"
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2009_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
--s 1000000 -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2010_v312/*.tif"
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2010_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
--s 1000000 -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2015_v312/*.tif"
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2015_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
--s 1000000 -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2016_v312/*.tif"
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2016_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
--s 1000000 -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2017_v312/*.tif"
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2017_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
--s 1000000 -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2018_v312/*.tif"
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2018_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
--s 1000000 -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2019_v312/*.tif"
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2019_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
 /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
--s 1000000 -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2020_v312/*.tif"
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2020_v312/*.tif"
+
+
+
+
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_1996_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2007_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2008_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2009_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2010_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2015_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2016_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2017_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2018_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2019_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_min_2020_v312/*.tif"
+
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_max_1996_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_max_2007_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_max_2008_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_max_2009_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_max_2010_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_max_2015_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_max_2016_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_max_2017_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_max_2018_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_max_2019_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_max_2020_v312/*.tif"
+
+
+
