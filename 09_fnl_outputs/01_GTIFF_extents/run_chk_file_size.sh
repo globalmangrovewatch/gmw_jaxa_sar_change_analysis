c977efd3c9d122b02299381e5d6066cd2506c76d
Pete Bunting <petebunting@mac.com>
2021-10-29 12:13:35 +0100

    fixed file name error

--- 09_fnl_outputs/01_GTIFF_extents/run_chk_file_size.sh
+++ 09_fnl_outputs/01_GTIFF_extents/run_chk_file_size.sh
@@ -1,43 +1,43 @@
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_file_st_size.py \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
 -s 1000000 -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_1996_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_file_st_size.py \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
 -s 1000000 -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2007_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_file_st_size.py \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
 -s 1000000 -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2008_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_file_st_size.py \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
 -s 1000000 -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2009_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_file_st_size.py \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
 -s 1000000 -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2010_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_file_st_size.py \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
 -s 1000000 -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2015_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_file_st_size.py \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
 -s 1000000 -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2016_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_file_st_size.py \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
 -s 1000000 -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2017_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_file_st_size.py \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
 -s 1000000 -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2018_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_file_st_size.py \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
 -s 1000000 -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2019_v312/*.tif"
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
-/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_file_st_size.py \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
 -s 1000000 -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/mng_v3_extent/gmw_v3_fnl_mjr_2020_v312/*.tif"
