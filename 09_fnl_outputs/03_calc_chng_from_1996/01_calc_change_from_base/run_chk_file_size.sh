429b491b6ddec054d847c82f9e9164bb88b871bd
Pete Bunting <petebunting@mac.com>
2021-10-29 13:14:43 +0100

    Update to check file sizes of other gtiff outputs.

---
+++ 09_fnl_outputs/03_calc_chng_from_1996/01_calc_change_from_base/run_chk_file_size.sh
@@ -0,0 +1,40 @@
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/gmw_chngs/gmw_v3_f1996_t2007_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/gmw_chngs/gmw_v3_f1996_t2008_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/gmw_chngs/gmw_v3_f1996_t2009_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/gmw_chngs/gmw_v3_f1996_t2010_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/gmw_chngs/gmw_v3_f1996_t2015_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/gmw_chngs/gmw_v3_f1996_t2016_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/gmw_chngs/gmw_v3_f1996_t2017_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/gmw_chngs/gmw_v3_f1996_t2018_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/gmw_chngs/gmw_v3_f1996_t2019_v312/*.tif"
+
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind \
+/home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python ../../00_utils/rm_files_gt_size.py \
+-s 1000000 --rmfile -i "/scratch/a.pfb/gmw_v3_change/data/fnl_v3_prods/gmw_chngs/gmw_v3_f1996_t2020_v312/*.tif"
+
