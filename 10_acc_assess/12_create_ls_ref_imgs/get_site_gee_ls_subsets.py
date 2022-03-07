e32c3b654281aa94f983c9c8f80a6943f7841829
Pete Bunting <petebunting@mac.com>
2022-03-07 12:29:07 +0000

    Add scripts which can be used to get landsat imagery from GEE.

---
+++ 10_acc_assess/12_create_ls_ref_imgs/get_site_gee_ls_subsets.py
@@ -0,0 +1,161 @@
+import ee
+import geemap
+import os
+import rsgislib
+import rsgislib.tools.filetools
+import rsgislib.imageutils
+
+
+def downloadFile(url, remote_path, local_path, time_out=None, username=None, password=None):
+    """
+    :param url:
+    :param remote_path:
+    :param local_path:
+    :param time_out: (default 300 seconds if None)
+    :param username:
+    :param password:
+    :return:
+    """
+    import pycurl
+    full_path_url = url+remote_path
+    success = False
+    try:
+        if time_out is None:
+            time_out = 300
+
+        fp = open(local_path, "wb")
+        curl = pycurl.Curl()
+        curl.setopt(pycurl.URL, full_path_url)
+        curl.setopt(pycurl.FOLLOWLOCATION, True)
+        curl.setopt(pycurl.NOPROGRESS, 0)
+        curl.setopt(pycurl.FOLLOWLOCATION, 1)
+        curl.setopt(pycurl.MAXREDIRS, 5)
+        curl.setopt(pycurl.CONNECTTIMEOUT, 50)
+        curl.setopt(pycurl.TIMEOUT, time_out)
+        curl.setopt(pycurl.FTP_RESPONSE_TIMEOUT, 600)
+        curl.setopt(pycurl.NOSIGNAL, 1)
+        if (not username is None) and (not password is None):
+            curl.setopt(pycurl.HTTPAUTH, pycurl.HTTPAUTH_ANY)
+            curl.setopt(pycurl.USERPWD, username + ':' + password)
+        curl.setopt(pycurl.WRITEDATA, fp)
+        print("Starting download of {}".format(full_path_url))
+        curl.perform()
+        print("Finished download in {0} of {1} bytes for {2}".format(curl.getinfo(curl.TOTAL_TIME), curl.getinfo(curl.SIZE_DOWNLOAD), full_path_url))
+        success = True
+    except:
+        print("An error occurred when downloading {}.".format(os.path.join(url, remote_path)))
+        success = False
+    return success
+
+
+def extract_data_gee(ls_row, ls_path, s_date, e_date, bbox, tmp_dir, out_img, landsat_col):
+    if not os.path.exists(tmp_dir):
+        os.mkdir(tmp_dir)
+    
+    Map = geemap.Map()
+    
+    collection = ee.ImageCollection(landsat_col) \
+      .filterDate(s_date, e_date) \
+      .filter(ee.Filter.eq('WRS_PATH', ls_path)) \
+      .filter(ee.Filter.eq('WRS_ROW', ls_row))
+    
+    # Compute the median in each band, each pixel.
+    # Band names are B1_median, B2_median, etc.
+    median = collection.reduce(ee.Reducer.median())
+    
+    # The output is an Image.  Add it to the map.
+    #vis_param = {'bands': ['B4_median', 'B5_median', 'B3_median'], 'opacity': 1.0, 'gamma': 1.2}
+    #Map.setCenter(-122.3355, 37.7924, 9)
+    #Map.addLayer(median, vis_param, 'Median Image')
+    
+    bbox_str = '[[{0}, {3}], [{1}, {3}], [{1}, {2}], [{0}, {2}]]'.format(*bbox)
+    print(bbox_str)
+    
+    url_path = median.getDownloadUrl({
+        'scale': 30,
+        'crs': 'EPSG:4326',
+        'region': bbox_str
+    })
+
+    #'[[-122.2, 37.7], [-122.0, 37.7], [-122.0, 37.5], [-122.2, 37.5]]'
+    
+    print(url_path)
+    
+    dwnld_file = os.path.join(tmp_dir, "download.zip")
+    downloadFile(url_path, "", local_path=dwnld_file)
+    
+        
+    rsgislib.tools.filetools.unzip_file(dwnld_file, out_dir=tmp_dir, gen_arch_dir=False, verbose=True)
+    
+    
+    #out_img = "r44_p34_2008_img.kea"
+    input_imgs = list()
+    input_imgs.append(rsgislib.tools.filetools.find_file_none(tmp_dir, "*B1*.tif"))
+    input_imgs.append(rsgislib.tools.filetools.find_file_none(tmp_dir, "*B2*.tif"))
+    input_imgs.append(rsgislib.tools.filetools.find_file_none(tmp_dir, "*B3*.tif"))
+    input_imgs.append(rsgislib.tools.filetools.find_file_none(tmp_dir, "*B4*.tif"))
+    input_imgs.append(rsgislib.tools.filetools.find_file_none(tmp_dir, "*B5*.tif"))
+    #input_imgs.append(rsgislib.tools.filetools.find_file_none(tmp_dir, "*B6*.tif"))
+    input_imgs.append(rsgislib.tools.filetools.find_file_none(tmp_dir, "*B7*.tif"))
+    rsgislib.imageutils.stack_img_bands(input_imgs, None, out_img, 0, 0, "KEA", rsgislib.TYPE_32FLOAT)
+    rsgislib.imageutils.pop_img_stats(out_img, use_no_data=True, no_data_val=0, calc_pyramids=True)
+
+
+
+def extract_data_gee_to_drive(ls_row, ls_path, s_date, e_date, bbox, out_img, out_folder, landsat_col):
+    
+    Map = geemap.Map()
+    
+    collection = ee.ImageCollection(landsat_col) \
+      .filterDate(s_date, e_date) \
+      .filter(ee.Filter.eq('WRS_PATH', ls_path)) \
+      .filter(ee.Filter.eq('WRS_ROW', ls_row))
+    
+    # Compute the median in each band, each pixel.
+    # Band names are B1_median, B2_median, etc.
+    median_img = collection.reduce(ee.Reducer.median())
+    
+    # The output is an Image.  Add it to the map.
+    #vis_param = {'bands': ['B4_median', 'B5_median', 'B3_median'], 'opacity': 1.0, 'gamma': 1.2}
+    #Map.setCenter(-122.3355, 37.7924, 9)
+    #Map.addLayer(median, vis_param, 'Median Image')
+    
+    #bbox_str = '[[{0}, {3}], [{1}, {3}], [{1}, {2}], [{0}, {2}]]'.format(*bbox)
+    #print(bbox_str)
+
+    
+    geom = ee.Geometry.Polygon(
+        [
+            [
+                [bbox[0], bbox[3]],
+                [bbox[1], bbox[3]],
+                [bbox[1], bbox[2]],
+                [bbox[0], bbox[2]],
+            ]
+        ]
+    )
+    feature = ee.Feature(geom, {})
+    roi = feature.geometry()
+    
+    
+    geemap.ee_export_image_to_drive(median_img, description=out_img, folder=out_folder, region=roi, scale=30)
+
+
+#extract_data_gee(ls_row=34, ls_path=44, s_date='2008-01-01', e_date='2008-12-31', bbox=[-122.2, -122.0, 37.5, 37.7], tmp_dir="r44_p34_2008_tmp", out_img="r44_p34_2008_img.kea", landsat_col='LANDSAT/LT05/C01/T1')
+extract_data_gee_to_drive(ls_row=34, ls_path=44, s_date='2008-01-01', e_date='2008-12-31', bbox=[-122.2, -122.0, 37.5, 37.7], out_img="r44_p34_2008_img_c2", out_folder='export_gmw_ls', landsat_col='LANDSAT/LT05/C02/T1_L2')
+
+
+
+
+
+
+
+
+# Center the map and display the image.
+#Map.centerObject(image, zoom=8)
+#Map.addLayer(image, {}, 'Landsat')
+
+#vis_params = {'bands': ['B5', 'B6', 'B4'], 'min': 0.0, 'max': 3000, 'opacity': 1.0, 'gamma': 1.2}
+#Map.addLayer(image, vis_params, "Landsat Vis")
+
+#Map
