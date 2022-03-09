1fff4b43044df735ee19d24b6078c9d2c113f581
Pete Bunting <petebunting@mac.com>
2022-03-09 11:12:54 +0000

    Fix error in set2 file naming error.

--- 10_acc_assess/14_create_acc_pts/create_acc_pts_sets.py
+++ 10_acc_assess/14_create_acc_pts/create_acc_pts_sets.py
@@ -15,6 +15,6 @@ for input_acc_pts_file in input_acc_pts_files:
     set1_out_vec_file = os.path.join(set1_out_dir, f"{set1_out_vec_lyr}.geojson")
 
     set2_out_vec_lyr = f"{input_acc_pts_lyr}_set2"
-    set2_out_vec_file = os.path.join(set2_out_dir, f"{set1_out_vec_lyr}.geojson")
+    set2_out_vec_file = os.path.join(set2_out_dir, f"{set2_out_vec_lyr}.geojson")
 
     rsgislib.vectorutils.split_vec_lyr_random_subset(input_acc_pts_file, input_acc_pts_lyr, set1_out_vec_file, set1_out_vec_lyr, set2_out_vec_file, set2_out_vec_lyr, n_smpl=250, out_format='GeoJSON', rnd_seed=None)
--- 10_acc_assess/15_calc_acc_stats/create_usr_pkgs.py
+++ 10_acc_assess/15_calc_acc_stats/create_usr_pkgs.py
@@ -1,24 +1,27 @@
 import os
+import rsgislib.tools.filetools
 
 def create_data_arch(site_lst, pts_dir, img_dirs, out_arch_file, tmp_dir):
-    print("Hello World")
     if not os.path.exists(tmp_dir):
         os.mkdir(tmp_dir)
 
     for site in site_lst:
         print(site)
+        site_pts_file = rsgislib.tools.filetools.find_file(pts_dir, f"gmw_chng_site_{site}_*.geojson")
+        print(site_pts_file)
 
-rl_sites = []
-ar_sites = []
-lh_sites = []
-nt_sites = []
+
+rl_sites = [3, 6,  29, 30, 31, 35]
+ar_sites = [1, 2,  11, 13, 20, 25, 28, 32, 33, 36, 38]
+lh_sites = [4, 7,  8,  9,  15, 18, 19, 21, 22, 26, 27]
+nt_sites = [5, 10, 12, 14, 16, 17, 23, 24, 34, 37]
 
 pts_dir = "acc_ref_pts_set_2"
 ls_imgs_dir = "/Users/pete/Dropbox/University/Research/Analysis/GlobalMangroveWatch/gmw_v3_acc/landsat_band_sub"
 
 
 create_data_arch(rl_sites, pts_dir, ls_imgs_dir, "richard_gmw_chng_sites_acc.tar.gz", "./tmp")
-create_data_arch(rl_sites, pts_dir, ar_sites, "ake_gmw_chng_sites_acc.tar.gz", "./tmp")
-create_data_arch(rl_sites, pts_dir, lh_sites, "lammert_gmw_chng_sites_acc.tar.gz", "./tmp")
-create_data_arch(rl_sites, pts_dir, nt_sites, "nathan_gmw_chng_sites_acc.tar.gz", "./tmp")
+#create_data_arch(rl_sites, pts_dir, ar_sites, "ake_gmw_chng_sites_acc.tar.gz", "./tmp")
+#create_data_arch(rl_sites, pts_dir, lh_sites, "lammert_gmw_chng_sites_acc.tar.gz", "./tmp")
+#create_data_arch(rl_sites, pts_dir, nt_sites, "nathan_gmw_chng_sites_acc.tar.gz", "./tmp")
 
