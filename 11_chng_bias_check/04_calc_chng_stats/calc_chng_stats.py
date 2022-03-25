ca19a8cd504bc514811abf5a8242853c39e63753
Pete Bunting <petebunting@mac.com>
2022-03-25 10:15:33 +0000

    Update with bug fix and summary stats

--- 11_chng_bias_check/04_calc_chng_stats/calc_chng_stats.py
+++ 11_chng_bias_check/04_calc_chng_stats/calc_chng_stats.py
@@ -53,10 +53,8 @@ for clmp_size in tqdm.tqdm([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]):
 
                 area_ha_m_nm = area_ha_col[(chng_cls_col == 3) & (hist_col >= clmp_size)]
                 chng_m_nm_areas["clmp_size"][clmp_size][chng_year].append(float(numpy.sum(area_ha_m_nm)))
-                area_ha_nm_m = area_ha_col[chng_cls_col == 4 & (hist_col >= clmp_size)]
+                area_ha_nm_m = area_ha_col[(chng_cls_col == 4) & (hist_col >= clmp_size)]
                 chng_nm_m_areas["clmp_size"][clmp_size][chng_year].append(float(numpy.sum(area_ha_nm_m)))
-
-
 
 rsgislib.tools.utils.write_dict_to_json(chng_m_nm_areas, "chng_areas_mangroves_notmangroves.json")
 rsgislib.tools.utils.write_dict_to_json(chng_nm_m_areas, "chng_areas_notmangroves_mangroves.json")
