1dc0c28fdcabb22a05528e4946998c43a22359e2
Pete Bunting <petebunting@mac.com>
2022-04-06 09:55:19 +0100

    Updated script to populate the accuracy points with v312-v315 classes.

--- 10_acc_assess/19_acc_ref_pts_pop_stats/02_pop_pt_sets.py
+++ 10_acc_assess/19_acc_ref_pts_pop_stats/02_pop_pt_sets.py
@@ -3,9 +3,6 @@ import glob
 import rsgislib.tools.filetools
 import rsgislib.classification
 
-versions = ["313", "314", "315"]
-
-
 
 acc_ref_pt_files = glob.glob("acc_ref_pts/*.geojson")
 acc_chng_pt_files = glob.glob("acc_chng_pts/*.geojson")
@@ -24,6 +21,7 @@ for acc_pt_file in acc_ref_pt_files:
     if not os.path.exists(chng_img):
         raise Exception("Change Image does not exist: {}".format(chng_img))
 
+    rsgislib.classification.pop_class_info_accuracy_pts(chng_img, vec_file=acc_pt_file, vec_lyr=basename, rat_class_col="class_names", vec_class_col="v312_cls")
 
 
 for acc_pt_file in acc_chng_pt_files:
@@ -38,7 +36,37 @@ for acc_pt_file in acc_chng_pt_files:
     if not os.path.exists(chng_img):
         raise Exception("Change Image does not exist: {}".format(chng_img))
 
+    rsgislib.classification.pop_class_info_accuracy_pts(chng_img, vec_file=acc_pt_file, vec_lyr=basename, rat_class_col="class_names", vec_class_col="v312_cls")
 
-#
 
-#rsgislib.classification.pop_class_info_accuracy_pts(input_img: str, vec_file: str, vec_lyr: str, rat_class_col: str, vec_class_col: str)
+versions = ["313", "314", "315"]
+
+for version in versions:
+    chng_imgs_path = os.path.join("../00_data/08_site_chng_maps", f"v{version}")
+    for acc_pt_file in acc_ref_pt_files:
+        basename = rsgislib.tools.filetools.get_file_basename(acc_pt_file)
+        basename_comps = basename.split('_')
+        site = basename_comps[3]
+        base_year = basename_comps[4]
+        chng_year = basename_comps[5]
+
+        print(f"{basename} = ({site}): {base_year} -- {chng_year}")
+        chng_img = os.path.join(chng_imgs_path, f"site_{site}", f"gmw_chng_site_{site}_{base_year}_{chng_year}.kea")
+        if not os.path.exists(chng_img):
+            raise Exception("Change Image does not exist: {}".format(chng_img))
+
+        rsgislib.classification.pop_class_info_accuracy_pts(chng_img, vec_file=acc_pt_file, vec_lyr=basename, rat_class_col="class_names", vec_class_col=f"v{version}_cls")
+
+    for acc_pt_file in acc_chng_pt_files:
+        basename = rsgislib.tools.filetools.get_file_basename(acc_pt_file)
+        basename_comps = basename.split('_')
+        site = basename_comps[1]
+        base_year = basename_comps[2]
+        chng_year = basename_comps[3]
+
+        print(f"{basename} = ({site}):  {base_year} -- {chng_year}")
+        chng_img = os.path.join(chng_imgs_path, f"site_{site}", f"gmw_chng_site_{site}_{base_year}_{chng_year}.kea")
+        if not os.path.exists(chng_img):
+            raise Exception("Change Image does not exist: {}".format(chng_img))
+
+        rsgislib.classification.pop_class_info_accuracy_pts(chng_img, vec_file=acc_pt_file, vec_lyr=basename, rat_class_col="class_names", vec_class_col=f"v{version}_cls")
