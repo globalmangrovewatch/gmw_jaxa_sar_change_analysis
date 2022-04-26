b1d0954df7de7e8ac464ee376596193c52d64fba
Pete Bunting <petebunting@mac.com>
2022-04-26 09:48:41 +0100

    Add scripts for updating min and max extents.

---
+++ 08_apply_fnl_qa/15_updated_min_max_extents/create_img_tile.py
@@ -0,0 +1,51 @@
+from pbprocesstools.pbpt_q_process import PBPTQProcessTool
+import logging
+import os
+import shutil
+import rsgislib
+import rsgislib.rastergis
+import rsgislib.imagecalc
+import rsgislib.vectorutils
+
+logger = logging.getLogger(__name__)
+
+class CreateImageTile(PBPTQProcessTool):
+
+    def __init__(self):
+        super().__init__(cmd_name='create_img_tile.py', descript=None)
+
+    def do_processing(self, **kwargs):
+        band_defns = [rsgislib.imagecalc.BandDefn('gmw', self.params['gmw_tile'], 1),
+                      rsgislib.imagecalc.BandDefn('min', self.params['min_ext_img'], 1)]
+        rsgislib.imagecalc.band_math(self.params['out_min_ext_img'], '(gmw==0)&&(min==1)?0:min', 'KEA', rsgislib.TYPE_8UINT, band_defns)
+        rsgislib.rastergis.pop_rat_img_stats(self.params['out_min_ext_img'], add_clr_tab=True, calc_pyramids=True, ignore_zero=True)
+
+        band_defns = [rsgislib.imagecalc.BandDefn('gmw', self.params['gmw_tile'], 1),
+                      rsgislib.imagecalc.BandDefn('max', self.params['max_ext_img'], 1)]
+        rsgislib.imagecalc.band_math(self.params['out_max_ext_img'], '(gmw==1)&&(max==0)?1:max', 'KEA', rsgislib.TYPE_8UINT, band_defns)
+        rsgislib.rastergis.pop_rat_img_stats(self.params['out_max_ext_img'], add_clr_tab=True, calc_pyramids=True, ignore_zero=True)
+
+
+    def required_fields(self, **kwargs):
+        return ["tile", "gmw_tile", "min_ext_img", "out_min_ext_img", "max_ext_img", "out_max_ext_img"]
+
+
+    def outputs_present(self, **kwargs):
+        files_dict = dict()
+        files_dict[self.params['out_min_ext_img']] = 'gdal_image'
+        files_dict[self.params['out_max_ext_img']] = 'gdal_image'
+        return self.check_files(files_dict)
+
+    def remove_outputs(self, **kwargs):
+        # Remove the output files.
+        if os.path.exists(self.params['out_min_ext_img']):
+            os.remove(self.params['out_min_ext_img'])
+
+        if os.path.exists(self.params['out_max_ext_img']):
+            os.remove(self.params['out_max_ext_img'])
+
+
+if __name__ == "__main__":
+    CreateImageTile().std_run()
+
+
